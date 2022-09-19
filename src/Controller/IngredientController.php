3558b025e99ca6941fe09d1ab3596cb9e947c2d2
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-09-19 22:53:50 +0200

    Added test Controllers
    Added auto-generated controller classes for Recipes, Instructions and Ingredients, including the Views and Forms.

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/ingredient')]
+class IngredientController extends AbstractController
+{
+    #[Route('/', name: 'app_ingredient_index', methods: ['GET'])]
+    public function index(IngredientRepository $ingredientRepository): Response
+    {
+        return $this->render('ingredient/index.html.twig', [
+            'ingredients' => $ingredientRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_ingredient_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, IngredientRepository $ingredientRepository): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredientRepository->add($ingredient, true);
+
+            return $this->redirectToRoute('app_ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ingredient/new.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_ingredient_show', methods: ['GET'])]
+    public function show(Ingredient $ingredient): Response
+    {
+        return $this->render('ingredient/show.html.twig', [
+            'ingredient' => $ingredient,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_ingredient_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ingredient $ingredient, IngredientRepository $ingredientRepository): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredientRepository->add($ingredient, true);
+
+            return $this->redirectToRoute('app_ingredient_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_ingredient_delete', methods: ['POST'])]
+    public function delete(Request $request, Ingredient $ingredient, IngredientRepository $ingredientRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ingredient->getId(), $request->request->get('_token'))) {
+            $ingredientRepository->remove($ingredient, true);
+        }
+
+        return $this->redirectToRoute('app_ingredient_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Controller/InstructionController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Instruction;
+use App\Form\InstructionType;
+use App\Repository\InstructionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/instruction')]
+class InstructionController extends AbstractController
+{
+    #[Route('/', name: 'app_instruction_index', methods: ['GET'])]
+    public function index(InstructionRepository $instructionRepository): Response
+    {
+        return $this->render('instruction/index.html.twig', [
+            'instructions' => $instructionRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_instruction_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, InstructionRepository $instructionRepository): Response
+    {
+        $instruction = new Instruction();
+        $form = $this->createForm(InstructionType::class, $instruction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $instructionRepository->add($instruction, true);
+
+            return $this->redirectToRoute('app_instruction_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('instruction/new.html.twig', [
+            'instruction' => $instruction,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_instruction_show', methods: ['GET'])]
+    public function show(Instruction $instruction): Response
+    {
+        return $this->render('instruction/show.html.twig', [
+            'instruction' => $instruction,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_instruction_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Instruction $instruction, InstructionRepository $instructionRepository): Response
+    {
+        $form = $this->createForm(InstructionType::class, $instruction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $instructionRepository->add($instruction, true);
+
+            return $this->redirectToRoute('app_instruction_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('instruction/edit.html.twig', [
+            'instruction' => $instruction,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_instruction_delete', methods: ['POST'])]
+    public function delete(Request $request, Instruction $instruction, InstructionRepository $instructionRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$instruction->getId(), $request->request->get('_token'))) {
+            $instructionRepository->remove($instruction, true);
+        }
+
+        return $this->redirectToRoute('app_instruction_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
