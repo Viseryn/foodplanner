8994bfa0a144188c79b63bc1d551e907aa76578c
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-09-28 00:52:06 +0200

    Added orphanRemoval to Model classes and additional cleanup

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -3,9 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\IngredientRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
