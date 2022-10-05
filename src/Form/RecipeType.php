05caf7d5788f258663db3d3643e66aad1d3c9d63
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-06 00:07:55 +0200

    Added image field to recipe forms

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -2,18 +2,35 @@
 
 namespace App\Form;
 
+use App\Entity\File;
 use App\Entity\Recipe;
+use App\Repository\FileRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RecipeType extends AbstractType
 {
+
+    public function __construct(FileRepository $fileRepository)
+    {
+        $this->fileRepository = $fileRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title')
             ->add('portionSize')
+            // Add all File objects that are images.
+            // The last two options allow for null as choice.
+            ->add('image', EntityType::class, [
+                'class' => File::class,
+                'choices' => $this->fileRepository->findAllImages(),
+                'required' => false,
+                'empty_data' => '',
+            ])
         ;
     }
 
