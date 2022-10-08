0c37a1755ac027570a1eea8111fe70082c30d689
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-08 05:18:18 +0200

    added basic FileUploader service

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class FileUploader 
+{
+    public function __construct($targetDirectory, SluggerInterface $slugger)
+    {
+        $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($originalFilename);
+        $filename = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+        try {
+            $file->move($this->getTargetDirectory(), $filename);
+        } catch (FileException $e) {
+            throw $e;
+        }
+
+        return $filename;
+    }
+
+    public function getTargetDirectory()
+    {
+        return $this->targetDirectory;
+    }
+}
