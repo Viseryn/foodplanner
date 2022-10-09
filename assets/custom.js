5d498286b202fe5c275f490b5e7d71822c76ff21
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-09 22:57:28 +0200

    added helper js function for recipe edit form

--- assets/custom.js
+++ assets/custom.js
@@ -6,3 +6,13 @@ $('.file-input').on('change', function(event) {
         .find('.label-content')
         .html(inputFile.files[0].name);
 });
+
+// Makes the image select button invisible if toggle is active.
+// Used in RecipeController::edit().
+$('#recipe_image_remove').on('change', function() {
+    if (this.checked) {
+        $('.file-label').addClass('invisible');
+    } else {
+        $('.file-label').removeClass('invisible');
+    }
+});
