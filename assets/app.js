d7f6fe0be7287dbaf237a0c387c4a8bc1bfadf72
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-09 18:49:03 +0200

    install jQuery and import it

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,7 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+// loads the jquery package from node_modules
+var $ = require('jquery');
+global.$ = global.jQuery = $;
