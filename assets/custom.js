5128a45c3aadd5f6739216cabeec78976a8974db
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-18 01:26:58 +0200

    added SweetAlert for delete forms

--- assets/custom.js
+++ assets/custom.js
@@ -16,3 +16,20 @@ $('#recipe_image_remove').on('change', function() {
         $('.file-label').removeClass('invisible');
     }
 });
+
+$('#delete-form').children('button[type="submit"]').on('click', function(e) {
+    e.preventDefault();
+    var form = $(this).parents('form');
+    swal({
+        dangerMode: true,
+        icon: 'error',
+        title: 'Permanently delete?',
+        text: 'You will not be able to recover deleted data.',
+        buttons: {
+            cancel: true,
+            confirm: 'Delete',
+        },
+    }).then((confirm) => {
+        if (confirm) form.trigger('submit');
+    });
+});
