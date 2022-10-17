4a3355cf744bc008d1ff5f8752acc2f09d5803fb
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-18 00:49:08 +0200

    added SASS and SweetAlert

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,13 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// sweetalert
+import swal from 'sweetalert';
 
 // loads the jquery package from node_modules
 var $ = require('jquery');
