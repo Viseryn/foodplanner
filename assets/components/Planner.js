67c212db47b12e4ec3754caacf04356f47b59e87
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-25 20:44:19 +0200

    Update Planner.js
    Added Sidebar functionality and added Spinner.

--- assets/components/Planner.js
+++ assets/components/Planner.js
@@ -2,15 +2,27 @@
     
 import React, {Component} from 'react';
 import {Route, Switch, Redirect, Link, withRouter} from 'react-router-dom';
+
+import Heading from './Heading';
+import Spinner from './Util';
     
 class Planner extends Component {
+    componentDidMount() {
+        this.props.updateSidebar('planner');
+        this.props.updateSAB(true, 'add', '/planner/add');
+    }
+
+    componentWillUnmount() {
+        this.props.updateSidebar();
+        this.props.updateSAB();
+    }
     
     render() {
-        loadSidebar('planner');
         return (
-            <div>
-                Planner
-            </div>
+            <>
+                <Heading title="Wochenplan" />
+                <Spinner />
+            </>
         )
     }
 }
