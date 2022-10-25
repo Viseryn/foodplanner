e36c23c896a8f0d900426c272c151090df579750
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-25 20:39:32 +0200

    Moved basic DOM structure to separate Component

--- assets/app.js
+++ assets/app.js
@@ -2,11 +2,7 @@
  * app.js
  */
 
-// loads the jquery package from node_modules
-// var $ = require('jquery');
-// global.$ = global.jQuery = $;
-
-// // load custom js
+// Load custom JavaScripts
 import './custom';
 
 // Import CSS
@@ -19,30 +15,10 @@ import './bootstrap';
 import swal from 'sweetalert';
 
 // Import React
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
-
-// Import React components
-import Sidebar from './components/Sidebar';
-import Planner from './components/Planner';
-import Recipes from './components/Recipes';
+import App from './components/App';
 
 // Render app
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Router>
-        <div className="flex bg-blue-50">
-            <Sidebar />
-
-            <div className="px-10 lg:mt-8 py-8 pb-28 lg:pb-8 lg:rounded-tl-3xl bg-white text-gray-700 w-full">
-                <div className="max-w-[700px]">
-                        <Routes>
-                            <Route path="/planner" element={<Planner />} />
-                            <Route path="/recipes" element={<Recipes />} />
-                        </Routes>
-                </div>
-            </div>
-        </div>
-    </Router>
-);
+root.render(<App />);
