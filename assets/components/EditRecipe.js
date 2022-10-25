f5f4b49a71363b6f8c5eedc39b41dc1a2d6f43c2
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-25 23:57:02 +0200

    Removed the "Remove image" toggle if the Recipe doesn't have an image

--- assets/components/EditRecipe.js
+++ assets/components/EditRecipe.js
@@ -321,11 +321,13 @@ export class EditRecipe extends Component {
                                             }
                                         </div>
 
-                                        <label htmlFor="recipe_image_remove" className="inline-flex relative items-center cursor-pointer">
-                                            <input type="checkbox" value="" id="recipe_image_remove" name="recipe[image_remove]" className="sr-only peer" onChange={(e) => this.handleFileRemove(e)} />
-                                            <div className="w-11 h-6 bg-gray-100 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all transition duration-300 peer-checked:bg-blue-600"></div>
-                                            <span className="ml-3 text-sm text-gray-500 font-semibold">Entfernen</span>
-                                        </label>
+                                        {this.state.recipe.image != null &&
+                                            <label htmlFor="recipe_image_remove" className="inline-flex relative items-center cursor-pointer">
+                                                <input type="checkbox" value="" id="recipe_image_remove" name="recipe[image_remove]" className="sr-only peer" onChange={(e) => this.handleFileRemove(e)} />
+                                                <div className="w-11 h-6 bg-gray-100 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all transition duration-300 peer-checked:bg-blue-600"></div>
+                                                <span className="ml-3 text-sm text-gray-500 font-semibold">Entfernen</span>
+                                            </label>
+                                        }
                                     </div>
                                 </div>
 
