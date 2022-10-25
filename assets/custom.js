b192039a5494e02ea7fcebf8b760e6a3bff3eae9
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-25 22:55:54 +0200

    Removed unused code

--- assets/custom.js
+++ assets/custom.js
@@ -6,17 +6,6 @@
 var $ = require('jquery');
 global.$ = global.jQuery = $;
 
-// Makes the image select button invisible if toggle is active.
-// Used in RecipeController::edit().
-$('#recipe_image_remove').on('change', function() {
-    if (this.checked) {
-        $('.file-label').addClass('invisible');
-    } else {
-        $('.file-label').removeClass('invisible');
-    }
-});
-
-
 /*********************
  * Utility Functions *
  *********************/
