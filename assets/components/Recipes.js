06b5f9c2d168b04cacf6ccbe28a473dd1f52ac1f
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-27 00:29:51 +0200

    Changed grid on medium sized screens

--- assets/components/Recipes.js
+++ assets/components/Recipes.js
@@ -82,7 +82,7 @@ export default class Recipes extends Component {
                         <Spinner />
                     ) : (
                         <>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-4 gap-4 mb-10">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
                                 {this.state.recipes.map(recipe => 
                                     <div 
                                         key={recipe.id}
