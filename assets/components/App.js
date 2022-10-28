ef08d7dc1ac6b20db322559806a43eaef9243635
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-28 21:23:50 +0200

    Fixed missing default value for the SAB label

--- assets/components/App.js
+++ assets/components/App.js
@@ -22,6 +22,7 @@ export default class App extends Component {
                 visible: false,
                 icon: '',
                 path: '#',
+                label: '',
             }
         };
     }
