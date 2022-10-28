beb4b1dd741990c94e9c7913c2de0879585af708
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-28 23:29:49 +0200

    Added the Update Days API

--- src/Controller/DayController.php
+++ src/Controller/DayController.php
@@ -72,6 +72,65 @@ class DayController extends AbstractController
         return (new JsonResponse($jsonContent));
     }
 
+    /**
+     * Update Days API
+     * 
+     * Deletes all past Day objects and creates new Day objects up 
+     * to ten days in the future.
+     *
+     * @param DayRepository $dayRepository
+     * @return Response
+     */
+    #[Route('/api/day/update', name: 'app_day_new', methods: ['GET'])]
+    public function updateDays(DayRepository $dayRepository): Response
+    {
+        $currentDays = $dayRepository->findBy([], ['timestamp' => 'ASC']);
+        $today = strtotime('today');
+
+        $newDays = [];
+
+        foreach ($currentDays as $day) {
+            if ($day->getTimestamp() < $today) {
+                // Delete all Day objects before today
+                $dayRepository->remove($day, true);
+            } else {
+                // Save timestamps of all currently existing Day objects
+                $newDays[] = $day->getTimestamp();
+            }
+        }
+
+        // Create a single Day object if $newDays is empty
+        if (count($newDays) === 0) {
+            $day = (new Day())->setTimestamp($today);
+
+            $dayRepository->add($day, true);
+            $newDays[] = $day->getTimestamp();
+        }
+
+        // Create new Day objects if there are now less than ten Day objects
+        if (count($newDays) < 10) {
+            while (count($newDays) < 10) {
+                $day = (new Day())->setTimestamp(strtotime('+1 day', end($newDays)));
+
+                $dayRepository->add($day, true);
+                $newDays[] = $day->getTimestamp();
+            }
+        }
+
+        // Delete everything after ten days in the future
+        if (count($newDays) > 10) {
+            $currentDays = $dayRepository->findBy([], ['timestamp' => 'ASC']);
+
+            for ($i = count($newDays) - 1; count($newDays) > 10; $i--) {
+                $dayRepository->remove($currentDays[$i], true);
+
+                array_pop($newDays);
+            }
+        }
+
+        return new Response();
+    }
+
     // #[Route('/new', name: 'app_day_new', methods: ['GET'])]
     // public function new(PlanUtil $planUtil, DayRepository $dayRepository): Response
     // {
