ae484da59dd89a4284aad0135dab9d4879d95e08
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-29 23:48:45 +0200

    Rewrote Recipes as a function component

--- assets/components/Recipes.js
+++ assets/components/Recipes.js
@@ -2,7 +2,7 @@
  * ./assets/components/Recipes.js *
  **********************************/
 
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 
@@ -17,99 +17,79 @@ import Button from './Buttons';
  * Collects the Recipe data from the Recipe List API
  * in the /src/Controller/RecipeController.php.
  */
-export default class Recipes extends Component {
-    /**
-     * constructor
-     * 
-     * Sets initial state variables.
-     * 
-     * @param {*} props 
-     */
-    constructor(props) {
-        super(props);
-        this.state = {recipes: [], loading: true};
-    }
+export default function Recipes(props) {
+    const [recipes, setRecipes] = useState([]);
+    const [isLoading, setLoading] = useState(true);
 
-    /**
-     * componentDidMount
-     * 
-     * Loads Recipes on mount and updates sidebar.
-     */
-    componentDidMount() {
-        this.props.setSidebarActiveItem('recipes');
-        this.props.setSidebarActionButton({
+    useEffect(() => {
+        // Load Sidebar
+        props.setSidebarActiveItem('recipes');
+        props.setSidebarActionButton({
             visible: true, 
             icon: 'add', 
             path: '/recipe/add', 
             label: 'Neues Rezept',
         });
+        
+        // Call the Recipe List API and load the Recipe
+        // data into the state variable.
+        axios
+            .get('/api/recipes')
+            .then(response => {
+                setRecipes(JSON.parse(response.data));
+                setLoading(false);
+            });
+    }, []);
+    
+    // Render
+    return (
+        <>
+            <Heading title="Rezepte" />
 
-        this.getRecipes();
-    }
 
-    /**
-     * getRecipe
-     * 
-     * Calls the Recipe List API and loads the Recipe
-     * data into the state variable.
-     */
-    getRecipes() {
-        axios.get('/api/recipes').then(
-            recipes => {
-                this.setState({ 
-                    recipes: JSON.parse(recipes.data), 
-                    loading: false
-                })
+            {isLoading 
+                ? <Spinner />
+                : <>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
+                        {recipesFiltered.map(recipe => 
+                            <RecipeCard key={recipe.id} recipe={recipe} />
+                        )}
+                    </div>
+                    
+                    <div className="flex justify-end">
+                        <div className="hidden md:block md:mt-10">
+                            <Button
+                                to="/recipe/add"
+                                icon="add"
+                                label="Neues Rezept"
+                                style="transparent"
+                            />
+                        </div>
+                    </div>
+                </>
             }
-        );
-    }
+        </>
+    );
+}
 
-    /**
-     * render
-     */
-    render() {
-        return (
-            <div>
-                <Heading title="Rezepte" />
-                {
-                    this.state.loading ? (
-                        <Spinner />
-                    ) : (
-                        <>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-10">
-                                {this.state.recipes.map(recipe => 
-                                    <div key={recipe.id} className="h-40 w-full shadow-md rounded-2xl transition duration-300 hover:shadow-2xl">
-                                        <div className="relative group">
-                                            <Link to={'/recipe/' + recipe.id}>
-                                                <img 
-                                                    className="rounded-2xl h-40 w-full object-cover brightness-[.7]" 
-                                                    src={recipe.image?.filename != null 
-                                                        ? recipe.image?.directory + recipe.image?.filename
-                                                        : '/img/default.jpg'
-                                                    } 
-                                                    alt={recipe.title}
-                                                />
-                                                <div className="absolute w-full bottom-4 px-6 text-white font-semibold text-xl">{recipe.title}</div>
-                                            </Link>
-                                        </div>
-                                    </div>
-                                )}
-                            </div>
-                            
-                            <div className="flex justify-end">
-                                <div className="hidden md:block md:mt-10">
-                                    <Button
-                                        to="/recipe/add"
-                                        icon="add"
-                                        label="Neues Rezept"
-                                        style="transparent"
-                                    />
-                                </div>
-                            </div>
-                        </>
-                    )
-                }
+function RecipeCard(props) {
+    return (
+        <div key={props.recipe.id} className="h-40 w-full shadow-md rounded-2xl transition duration-300 hover:shadow-2xl">
+            <div className="relative group">
+                <Link to={'/recipe/' + props.recipe.id}>
+                    <img 
+                        className="rounded-2xl h-40 w-full object-cover brightness-[.7]" 
+                        src={props.recipe.image?.filename != null 
+                            ? props.recipe.image?.directory + props.recipe.image?.filename
+                            : '/img/default.jpg'
+                        } 
+                        alt={props.recipe.title}
+                    />
+                    <div className="absolute w-full bottom-4 px-6 text-white font-semibold text-xl">
+                        {props.recipe.title}
+                    </div>
+                </Link>
             </div>
-        )
-    }
+        </div>
+    );
 }
