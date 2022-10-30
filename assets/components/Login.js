6d211cc61e8c56942cf7922b0a07a80a9ce728ff
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-30 23:52:58 +0100

    Created Login component

---
+++ assets/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Login() {
+    
+
+    /**
+     * Render
+     */
+    return (
+        <div className="px-6 pb-24 pt-6 md:pb-6 md:my-6 md:mr-6 w-full min-h-screen md:min-h-fit bg-white dark:bg-[#1D252C] md:rounded-3xl md:max-w-[900px]">
+            Login
+        </div>
+    );
+}
