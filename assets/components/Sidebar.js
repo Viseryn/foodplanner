087f446fc9488f795158a6c7b5f732e63a8d18a0
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-31 02:24:57 +0100

    Added Sidebar Drawer

--- assets/components/Sidebar.js
+++ assets/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 /**
@@ -9,12 +9,41 @@ import { Link } from 'react-router-dom';
  */
 
 export default function Sidebar(props) {
+    const [isDrawerVisible, setDrawerVisible] = useState(false);
+
     return (
-        <aside className="z-50 bg-blue-50 dark:bg-[#1D252C] shrink-0 h-20 w-full md:w-24 md:min-w-24 md:min-h-screen xl:w-64 fixed bottom-0 md:static md:flex md:justify-center xl:justify-start">
-            <div className="pl-4 pr-6 py-3 w-full fixed flex justify-between md:px-6 md:py-7 md:max-w-fit md:block xl:w-64 xl:max-w-none">
-                <SidebarContent {...props} />
+        <>
+            {/* SidebarDrawer */}
+            <aside className={
+                'z-50 fixed h-full w-full ease-in-out duration-300' 
+                + (isDrawerVisible ? '' : ' -translate-x-full')
+            }>
+                <div className="bg-white dark:bg-[#29353f] rounded-r-3xl h-full w-64 flex justify-between px-6 py-7">
+                    <ul className="space-y-2 mb-16 hidden md:block">
+                        <SidebarDrawerButton
+                            isDrawerVisible={isDrawerVisible}
+                            setDrawerVisible={setDrawerVisible} 
+                        />
+                    </ul>
+                </div>
+            </aside>
+            <div className={
+                'bg-gradient-to-r from-black/75 h-full w-full z-40 fixed duration-300'
+                + (isDrawerVisible ? '' : ' opacity-0 -translate-x-full')
+            }>
             </div>
-        </aside>
+
+            {/* Sidebar */}
+            <aside className="z-30 bg-blue-50 dark:bg-[#1D252C] shrink-0 h-20 w-full md:w-24 md:min-w-24 md:min-h-screen xl:w-64 fixed bottom-0 md:static md:flex md:justify-center xl:justify-start">
+                <div className="pl-4 pr-6 py-3 w-full fixed flex justify-between md:px-6 md:py-7 md:max-w-fit md:block xl:w-64 xl:max-w-none">
+                    <SidebarContent 
+                        isDrawerVisible={isDrawerVisible}
+                        setDrawerVisible={setDrawerVisible} 
+                        {...props} 
+                    />
+                </div>
+            </aside>
+        </>
     );
 }
 
@@ -22,16 +51,10 @@ function SidebarContent(props) {
     return (
         <>
             <ul className="space-y-2 mb-16 hidden md:block">
-                <li className="xl:w-min">
-                    <Link 
-                        className="flex items-center p-4 text-gray-900 rounded-full transition duration-300 hover:bg-blue-100 active:bg-blue-200 active:scale-90 group"
-                        to="#"
-                    >
-                        <span className="material-symbols-rounded text-gray-500 transition duration-300 group-hover:text-gray-900">
-                            menu
-                        </span>
-                    </Link>
-                </li>
+                <SidebarDrawerButton
+                    isDrawerVisible={props.isDrawerVisible}
+                    setDrawerVisible={props.setDrawerVisible} 
+                />
                 
                 <SidebarActionButton sidebarActionButton={props.sidebarActionButton} />
             </ul>
@@ -49,7 +72,7 @@ function SidebarContent(props) {
                     icon="fastfood"
                     label="Rezepte"
                 />
-                {/* <SidebarItem 
+                <SidebarItem 
                     sidebarActiveItem={props.sidebarActiveItem}
                     id="pantry"
                     icon="kitchen"
@@ -60,13 +83,29 @@ function SidebarContent(props) {
                     id="shoppinglist"
                     icon="shopping_cart"
                     label="Einkaufsliste"
-                /> */}
+                />
             </ul>
                 
             <ul className="md:hidden">
                 <SidebarActionButton sidebarActionButton={props.sidebarActionButton} />
             </ul>
         </>
+    );
+}
+
+function SidebarDrawerButton(props) {
+    return (
+        <li className="xl:w-min">
+            <Link 
+                className="flex items-center p-4 rounded-full transition duration-300 hover:bg-blue-200 dark:hover:bg-[#1f3953] active:bg-blue-200 active:scale-90 group"
+                to="#"
+                onClick={() => props.setDrawerVisible(!props.isDrawerVisible)}
+            >
+                <span className="material-symbols-rounded transition duration-300 text-gray-600 dark:text-gray-300 group-hover:text-gray-900 dark:group-hover:text-gray-200">
+                    menu
+                </span>
+            </Link>
+        </li>
     );
 }
 
