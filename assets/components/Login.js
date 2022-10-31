77e9fa9462366bdbbec11252e023c39ed24b37de
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-31 05:06:26 +0100

    Created working Login component

--- assets/components/Login.js
+++ assets/components/Login.js
@@ -1,14 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Login() {
-    
+import { SubmitButton } from './Buttons';
+import Heading from './Heading';
+import InputRow from './Forms';
+import Notification from './Notification';
+import Spinner from './Util';
+
+/**
+ * Login
+ * 
+ * 
+ */
+export default function Login(props) {
+    // State variables
+    const [isLoading, setLoading] = useState(true);
+    const [response, setResponse] = useState();
+    const [user, setUser] = useState();
+
+    // Load sidebar and user data
+    useEffect(() => {
+        props.setSidebarActiveItem();
+        props.setSidebarActionButton();
+
+        getUser();
+    }, []);
+
+    /**
+     * handleSubmit
+     * 
+     * On submitting the form, calls the Login API.
+     * Responds with an error on failure.
+     */
+    const handleSubmit = (event) => {
+        const formData = new FormData(event.target);
+        event.preventDefault();
+
+        setResponse();
+        setLoading(true);
+
+        axios
+            .post('/api/login', formData)
+            .then(response => {
+                setResponse(response.data);
+                getUser();
+            });
+    };
+
+    /**
+     * getUser
+     * 
+     * Calls the User API, which responds with an 
+     * array containing the username and user roles
+     * (if authenticated), or an empty array elsewise.
+     */
+    const getUser = () => {
+        axios
+            .get('/api/user')
+            .then(response => {
+                setLoading(false);
+                setUser(JSON.parse(response.data));
+            });
+    }
 
     /**
      * Render
      */
     return (
-        <div className="px-6 pb-24 pt-6 md:pb-6 md:my-6 md:mr-6 w-full min-h-screen md:min-h-fit bg-white dark:bg-[#1D252C] md:rounded-3xl md:max-w-[900px]">
-            Login
+        <div className="px-6 pb-24 pt-6 md:pb-6 md:my-6 md:mr-6 w-full min-h-screen md:min-h-fit bg-white dark:bg-[#29353f] md:rounded-3xl md:w-fit">
+            <Heading>Login</Heading>
+            <div className="min-w-[400px]"></div>
+            
+            {isLoading &&
+                <Spinner />
+            }
+
+            {response?.error &&
+                <div className="mb-10">
+                    <Notification
+                        title="Login fehlgeschlagen!"
+                        message={'Fehlercode: ' + response?.error}
+                        color="red"
+                    />
+                </div>
+            }
+
+            {user?.username !== undefined &&
+                <Notification
+                    title="Erfolgreich eingeloggt!"
+                    message={'Willkommen, ' + user?.username + '.'}
+                    color="green"
+                />
+            }
+
+            {user?.username === undefined && !isLoading &&
+                <form onSubmit={handleSubmit} className="sm:w-[400px]">
+                    <InputRow 
+                        id="username"
+                        label="Dein Benutzername"
+                        inputProps={{
+                            required: 'required', 
+                            name: '_username',
+                        }}
+                    />
+                    <InputRow 
+                        id="password"
+                        label="Dein Passwort"
+                        inputProps={{
+                            required: 'required', 
+                            type: 'password',
+                            name: '_password',
+                            placeholder: '••••••••',
+                        }}
+                    />
+
+                    <div className="flex justify-end">
+                        <SubmitButton
+                            icon="login"
+                            label="Einloggen"
+                        />
+                    </div>
+                </form>
+            }
         </div>
     );
 }
