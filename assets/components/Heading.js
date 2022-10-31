4b654b6fc2f420a755ec83ea9f30692277ccf697
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-10-31 05:07:17 +0100

    Updated Heading and added SubHeading

--- assets/components/Heading.js
+++ assets/components/Heading.js
@@ -3,7 +3,16 @@ import React, {Component} from 'react';
 export default function Heading(props) {
     return (
         <div className="text-3xl font-semibold text-blue-600 dark:text-gray-100 mb-10">
-            {props.title || ''}
+            {props.title || ''} 
+            {props.children}
+        </div>
+    );
+}
+
+export function SubHeading(props) {
+    return (
+        <div className="text-lg font-semibold text-blue-600 dark:text-gray-100 mb-10">
+            {props.children}
         </div>
     );
 }
