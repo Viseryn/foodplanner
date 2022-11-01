d1fb4032b6879db8067279dd455dcaf95d0f31fd
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-11-01 03:21:09 +0100

    Added container

--- assets/components/Pantry.js
+++ assets/components/Pantry.js
@@ -16,9 +16,9 @@ export default function Pantry(props) {
     }, []);
 
     return (
-        <>
-            <Heading title="Vorratskammer" />
+        <div className="px-6 pb-24 pt-6 md:pb-6 md:my-6 md:mr-6 w-full min-h-screen md:min-h-fit bg-white dark:bg-[#29353f] md:rounded-3xl md:w-[450px]">
+            <Heading>Vorratskammer</Heading>
             <Spinner />
-        </>
+        </div>
     );
 }
