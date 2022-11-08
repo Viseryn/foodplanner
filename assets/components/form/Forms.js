ab801fdc1c2c5597e34a72b0236e0fd27a0ebe09
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-11-08 02:54:58 +0100

    Cleanup

--- assets/components/form/Forms.js
+++ assets/components/form/Forms.js
@@ -5,10 +5,9 @@
 /**
  * Global styles
  */
-let inputRowStyle       = 'mb-6';
-let inputLabelStyle     = 'text-sm font-semibold block mb-2';
-let inputWidgetStyle    = 'dark:placeholder-gray-400 dark:bg-[#1D252C] border border-gray-300 dark:border-none rounded-full h-10 px-6 shadow-sm dark:shadow-md w-full transition duration-300 focus:border-blue-600';
-let textareaWidgetStyle = 'dark:placeholder-gray-400 dark:bg-[#1D252C] block border border-gray-300 dark:border-none rounded-xl px-4 py-2 shadow-sm dark:shadow-md w-full transition duration-300 focus:border-blue-600';
+const inputRowStyle    = 'mb-6';
+const inputLabelStyle  = 'text-sm font-semibold block mb-2';
+const inputWidgetStyle = 'dark:placeholder-gray-400 dark:bg-[#1D252C] border border-gray-300 dark:border-none rounded-full h-10 px-6 shadow-sm dark:shadow-md w-full transition duration-300 focus:border-blue-600';
 
 /**
  * nameFromId
@@ -21,7 +20,7 @@ let textareaWidgetStyle = 'dark:placeholder-gray-400 dark:bg-[#1D252C] block bor
 function nameFromId(id) {
     let arr = id?.split("_");
     let str = (arr?.length > 1) ? arr[0] + '[' + arr[1] + ']' : '';
-
+    
     return str;
 }
 
@@ -29,4 +28,4 @@ function nameFromId(id) {
  * Export functions and variables
  */
 export { nameFromId };
-export { inputRowStyle, inputLabelStyle, inputWidgetStyle, textareaWidgetStyle };
+export { inputRowStyle, inputLabelStyle, inputWidgetStyle };
