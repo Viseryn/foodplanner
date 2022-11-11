e10ec8feabb7da088e08f4d98aac40266df6d40f
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-11-12 00:01:15 +0100

    Added ShoppingList to global state

--- assets/layouts/App.js
+++ assets/layouts/App.js
@@ -6,20 +6,17 @@ import React, { useEffect, useState } from "react";
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import axios from "axios";
 
-import Sidebar from '../layouts/Sidebar';
+import generateDisplayName from '../util/generateDisplayName';
 
+import Sidebar from '../layouts/Sidebar';
 import Planner from '../pages/Planner/Planner';
 import AddMeal from '../pages/Planner/AddMeal';
-
 import Recipes from '../pages/Recipes/Recipes';
 import AddRecipe from '../pages/Recipes/AddRecipe';
 import EditRecipe from '../pages/Recipes/EditRecipe';
-
 import ShoppingList from '../pages/ShoppingList/ShoppingList';
-
 import Login from '../pages/Login/Login';
 import Logout from '../pages/Logout/Logout';
-
 import PageNotFound from '../pages/PageNotFound/PageNotFound';
 
 /**
@@ -157,6 +154,61 @@ export default function App() {
         }
     }, [isLoadingDays]);
 
+    /**
+     * Keep shopping list in global state variable
+     * and pass it as props to subcomponents.
+     */
+    const [shoppingList, setShoppingList] = useState([]);
+    const [isLoadingShoppingList, setLoadingShoppingList] = useState(true);
+
+    const setShoppingListProps = {
+        'shoppingList': shoppingList,
+        'setShoppingList': setShoppingList,
+        'isLoadingShoppingList': isLoadingShoppingList,
+        'setLoadingShoppingList': setLoadingShoppingList,
+    };
+
+    /**
+     * Load shopping list into global state when 
+     * isLoadingShoppingList is true, e.g. on first render.
+     */
+    useEffect(() => {
+        if (isLoadingShoppingList) {
+            axios
+                .get('/api/shoppinglist')
+                .then(response => {
+                    let itemsRaw = JSON.parse(response.data);
+
+                    itemsRaw.forEach(item => {
+                        // Save original name of each item
+                        item.originalName = item.name;
+
+                        // Add quantity to the name field, which will be displayed.
+                        // The Shopping List Update API will split everything later.
+                        item.name = generateDisplayName(
+                            item.quantity_value, 
+                            item.quantity_unit, 
+                            item.originalName
+                        );
+                    });
+
+                    // Order list by position
+                    const compareItems = (a, b) => {
+                        if (a.position < b.position) return -1;
+                        if (a.position > b.position) return 1;
+                        return 0;
+                    }
+
+                    itemsRaw.sort(compareItems);
+
+                    // Add list to state
+                    setShoppingList(itemsRaw);
+                    setLoadingShoppingList(false);
+                })
+            ;
+        }
+    }, [isLoadingShoppingList]);
+
     /** 
      * Render
      */
@@ -173,20 +225,23 @@ export default function App() {
                     <Route path="/"                 element={<Planner       {...setSidebarProps} 
                                                                             {...setDaysProps} />} />
                     <Route path="/planner"          element={<Planner       {...setSidebarProps} 
-                                                                            {...setDaysProps} />} />
+                                                                            {...setDaysProps}
+                                                                            {...setShoppingListProps} />} />
                     <Route path="/planner/add"      element={<AddMeal       {...setSidebarProps} 
                                                                             {...setDaysProps} 
                                                                             {...setRecipesProps} />} />
                     <Route path="/planner/add/:id"  element={<AddMeal       {...setSidebarProps} 
                                                                             {...setDaysProps} 
                                                                             {...setRecipesProps} />} />
-                    <Route path="/shoppinglist"     element={<ShoppingList  {...setSidebarProps} />} />
+                    <Route path="/shoppinglist"     element={<ShoppingList  {...setSidebarProps}
+                                                                            {...setShoppingListProps} />} />
                     <Route path="/recipes"          element={<Recipes       {...setSidebarProps} 
                                                                             {...setRecipesProps} />} />
                     <Route path="/recipe/add"       element={<AddRecipe     {...setSidebarProps} 
                                                                             {...setRecipesProps} />} />
                     <Route path="/recipe/:id"       element={<Recipes       {...setSidebarProps} 
-                                                                            {...setRecipesProps} />} />
+                                                                            {...setRecipesProps}
+                                                                            {...setShoppingListProps} />} />
                     <Route path="/recipe/:id/edit"  element={<EditRecipe    {...setSidebarProps} 
                                                                             {...setRecipesProps} />} />
                     <Route path="/login"            element={<Login         {...setSidebarProps} 
