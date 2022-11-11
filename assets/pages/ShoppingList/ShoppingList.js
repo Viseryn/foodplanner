5dd71fe892a4894d1fd2514345d59fdd16927085
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-11-12 00:19:50 +0100

    Moved generateDisplayName into own file

--- assets/pages/ShoppingList/ShoppingList.js
+++ assets/pages/ShoppingList/ShoppingList.js
@@ -5,6 +5,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
+import generateDisplayName from '../../util/generateDisplayName';
+
 import AddItemInputWidget from './components/AddItemInputWidget'
 import Heading from '../../components/ui/Heading';
 import Spinner from '../../components/ui/Spinner';
@@ -118,31 +120,6 @@ export default function ShoppingList(props) {
         });
 
         return returnVal;
-    };
-
-    /**
-     * generateDisplayName
-     * 
-     * Generates the displayed name of a shopping list item,
-     * which includes the quantity of the ingredient.
-     * 
-     * @param {string} quantity_value
-     * @param {string} quantity_unit
-     * @param {string} originalName
-     * @return Returns the display name of an item.
-     */
-    const generateDisplayName = (quantity_value, quantity_unit, originalName) => {
-        let quantity = quantity_value;
-
-        if (quantity && quantity_unit) {
-            quantity += ' ' + quantity_unit;
-        } else if (quantity_unit) {
-            quantity += quantity_unit;
-        }
-
-        const displayName = (quantity ? quantity + ' ' : '') + originalName;
-
-        return displayName;
     };
 
     /**
