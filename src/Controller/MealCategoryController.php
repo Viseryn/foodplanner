6f56818450364956e20b2656c3128be419da219c
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-11-21 18:21:09 +0100

    Updated MealCategories API

--- src/Controller/MealCategoryController.php
+++ src/Controller/MealCategoryController.php
@@ -30,9 +30,25 @@ class MealCategoryController extends AbstractController
         // Fetch all MealCategory objects from the database
         $mealCategories = $mealCategoryRepository->findAll();
 
+        // Create array for response
+        $mealCategoriesResponse = [];
+
+        // Add public data to the response array
+        foreach ($mealCategories as $mealCategory) {
+            $mealCategoriesResponse[] = [
+                'name' => $mealCategory->getName(),
+                'isStandard' => $mealCategory->isStandard(),
+                'icon' => $mealCategory->getIcon(),
+                'id' => 'mealCategory_' . $mealCategory->getName(),           // For radio buttons
+                'value' => $mealCategory->getId(),                         // For radio buttons
+                'label' => $mealCategory->getName(),                       // For radio buttons
+                'checked' => $mealCategory->isStandard() ? 'checked' : '', // For radio buttons
+            ];
+        }
+
         // Serialize data and respond
         $serializer = SerializerBuilder::create()->build();
-        $jsonContent = $serializer->serialize($mealCategories, 'json');
+        $jsonContent = $serializer->serialize($mealCategoriesResponse, 'json');
 
         return new JsonResponse($jsonContent);
     }
@@ -58,7 +74,7 @@ class MealCategoryController extends AbstractController
             $setStandard = false;
 
             // Get UserGroup from db
-            $categoryDb = $mealCategoryRepository->find($category->id);
+            $categoryDb = $mealCategoryRepository->find($category->value);
             
             if ($category->standard && !$setStandard) {
                 $categoryDb->setStandard(true);
