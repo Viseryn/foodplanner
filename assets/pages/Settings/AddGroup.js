e96fded08b623a063221b6749151aea23cf74349
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-12-07 23:28:31 +0100

    Removed local authentication check

--- assets/pages/Settings/AddGroup.js
+++ assets/pages/Settings/AddGroup.js
@@ -88,10 +88,6 @@ export default function AddGroup(props) {
      */
     return (
         <>
-            {!props.isLoadingUser && props.user?.username === undefined &&
-                <Navigate to="/login" />
-            }
-
             {isSubmitted && <Navigate to={'/settings'} />}
 
             <div className="px-6 pb-24 pt-6 md:pb-6 md:my-6 md:mr-6 w-full min-h-screen md:min-h-fit bg-white dark:bg-[#29353f] md:rounded-3xl md:w-[450px]">
