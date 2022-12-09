3d0be4040a8224a33d3660e36b246d3196dc03f5
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-12-09 02:12:08 +0100

    Added documentation

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -20,58 +20,77 @@ class FileUploader
         $this->fileRepository = $fileRepository;
     }
 
-    public function getTargetDirectory()
+    /**
+     * getTargetDirectory
+     *
+     * @return string The configured targetDirectory in /config/services.yaml.
+     */
+    public function getTargetDirectory(): string
     {
         return $this->targetDirectory;
     }
 
     /**
-     * Uploads a file to a specific directory.
+     * upload
+     * 
+     * Uploads a file to a specified directory.
      * If $public is true, the file is uploaded to /public/$directory/,
-     * otherwise to /data/upload/.
+     * otherwise to /data/upload/$directory/.
      *
      * @param UploadedFile $file File data, e.g. input delivered through a form.
-     * @param string|null $dir Addition to the configured $targetDirectory, e.g. /img/.
+     * @param string|null $directory Addition to the configured $targetDirectory, e.g. /img/.
+     * @param boolean $public Whether the file should be uploaded to a public directory.
      * @throws FileException If file could not be uploaded to the desired directory.
-     * @return File
+     * @return File The File objects corresponding to the uploaded file.
      */
-    public function upload(UploadedFile $file, ?string $directory, ?bool $public = true): File
+    public function upload(UploadedFile $file, ?string $directory, bool $public = true): File
     {
+        // Create a safe filename from the original filename
         $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
         $safeFilename = $this->slugger->slug($originalFilename);
         $filename = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
 
+        // Specify the upload directory
         $uploadDir = $this->getTargetDirectory() 
             . ($public ? '/public/' : '/data/upload/') 
             . $directory;
 
+        // Specify the directory property for the File object and delete unnecessary slashes
         $objectDir = '/' . $directory . '/';
         $objectDir = preg_replace('#/+#', '/', $objectDir);
 
         try {
+            // Try uploading the file
             $file->move($uploadDir, $filename);
 
+            // Create File object
             $fileObject = new File();
             $fileObject
                 ->setFilename($filename)
                 ->setDirectory($objectDir)
-                ->setPublic($public);
+                ->setPublic($public)
+            ;
 
+            // Add File object to database
             $this->fileRepository->add($fileObject, true);
         } catch (FileException $e) {
+            // Throw FileException if file could not be uploaded.
             throw $e;
         }
 
+        // Return File object.
         return $fileObject;
     }
 
     /**
+     * getExtension
+     * 
      * Returns the extension of a File object 
      * or a string that is a filename (with or 
      * without directory).
      *
-     * @param File|string $file
-     * @return string|null
+     * @param File|string $file A File object or a filename.
+     * @return string|null The extension of the file.
      */
     public function getExtension(File|string $file): ?string 
     {
@@ -85,11 +104,13 @@ class FileUploader
     }
 
     /**
+     * isImage
+     * 
      * Checks whether a File object or a string 
      * that contains a filename is an image file.
      *
-     * @param File|string $file
-     * @return boolean Returns true if $file is an image.
+     * @param File|string $file A File object or a filename.
+     * @return boolean Returns true if $file is an image and false otherwise.
      */
     public function isImage(File|string $file): bool
     {
