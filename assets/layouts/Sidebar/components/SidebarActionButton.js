8c18a3a5e288ed523eefea537e51d215f08f3829
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2022-12-30 19:12:02 +0100

    Update SidebarActionButton.js
    - Added an animation for the floating SAB
    - Uses the scrollDirection hook for the SAB label
    - Cleaned up the styling of the SAB

--- assets/layouts/Sidebar/components/SidebarActionButton.js
+++ assets/layouts/Sidebar/components/SidebarActionButton.js
@@ -4,6 +4,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import useScrollDirection from '../../../hooks/useScrollDirection';
 
 /**
  * SidebarActionButton
@@ -16,57 +17,69 @@ import { Link, useLocation } from 'react-router-dom';
  * @property {?function} onClickHandler An optional onClickHandler for the SAB.
  * @property {string} icon The icon of the SAB.
  * @property {string} label The label of the SAB.
- * @property {?boolean} floating If true, the SAB will be displayed extended and floating in the bottom-right corner.
+ * @property {?boolean} floating Set true for being displayed expanded and floating in the bottom-right corner. Set false for an SAB integrated in the sidebar.
  * 
  * @example 
  * <SidebarActionButton sidebarActionButton={props.sidebarActionButton} />
  */
 export default function SidebarActionButton(props) {
-    const SABbaseStyle = 'flex items-center p-4 rounded-2xl transition duration-300 h-14 xl:w-auto' + (props.floating ? ' w-auto fixed bottom-[6.5rem] right-6 shadow-xl' : ' w-14');
-    const SABinvisibleStyle = SABbaseStyle + ' hidden';
-    const SABvisibleStyle = SABbaseStyle + ' text-gray-900 dark:text-gray-300 bg-pink-300 dark:bg-pink-800 hover:bg-pink-400 dark:hover:bg-pink-600 active:bg-pink-500 active:scale-90';
+    /**
+     * Make the SAB label visible when the user scrolls up or is at top of page
+     */
+    const scrollDirection = useScrollDirection();
+    const isLabelVisible = scrollDirection === 'up' || window.pageYOffset === 0;
 
     /**
-     * Helpers for visibility of SAB label on mobile
+     * SAB styling classes
      */
-    const [isVisible, setVisible] = useState(true);
-    const [scrollY, setScrollY] = useState(0);
-  
-    useEffect(() => {   
-      window.addEventListener("scroll", listenToScroll);
-      return () => window.removeEventListener("scroll", listenToScroll); 
-    }, []);
-
-    const listenToScroll = () => {
-        const heightToHideFrom = 1; // Removes label of SAB after scrolling past 1px
-        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        setScrollY(winScroll);
-
-        if (winScroll > heightToHideFrom) {  
-            isVisible && setVisible(false);
-        } else {
-            setVisible(true);
-        }  
+    const styles = {
+        base: 'flex items-center p-4 rounded-2xl transition duration-300 h-14'
+            + ' text-gray-900 dark:text-gray-300 bg-pink-300 dark:bg-pink-800 hover:bg-pink-400'
+            + ' dark:hover:bg-pink-600 active:bg-pink-500 active:scale-90',
+        smSize: ' shadow-xl transition-all duration-300 overflow-hidden', 
+        smSizeReduced: ' max-w-[56px]',
+        smSizeExpanded: ' max-w-[250px]',
+        label: 'pl-4 font-semibold whitespace-nowrap',
     };
+
+    /** 
+     * Build styling class for the button itself (the <Link /> element).
+     * If the configuration sidebarActionButton.visible === false, then
+     * the element is just hidden. Otherwise, use the base styling.
+     * For a floating action button (i.e., on small screens), we 
+     * add the styling styles.smSize. The label is hidden when scrolling 
+     * down, so we animate the width of the action button with the 
+     * two stylings styles.smSizeExpanded and styles.smSizeReduced.
+     */
+    const buttonStyle = !props.sidebarActionButton?.visible 
+        ? 'hidden'
+        : styles.base + (props.floating 
+            ? styles.smSize + (isLabelVisible 
+                ? styles.smSizeExpanded 
+                : styles.smSizeReduced)
+            : ''
+        )
+    ;
+
+    /**
+     * Build styling class for the label.
+     * For a non-floating action button (i.e., on larger screens), 
+     * the label only becomes visible at xl-size screens.
+     */
+    const labelStyle = styles.label + (!props.floating ? ' hidden xl:block' : '');
 
     /**
      * Render
      */
     return (
-        <li className="sidebar-action-button h-14">
+        <li className="h-14">
             <Link 
                 to={props.sidebarActionButton?.path}
-                className={props.sidebarActionButton?.visible ? SABvisibleStyle : SABinvisibleStyle}
                 onClick={props.sidebarActionButton?.onClickHandler}
+                className={buttonStyle}
             >
-                <span className="material-symbols-rounded">
-                    {
-                        props.sidebarActionButton?.visible 
-                        ? props.sidebarActionButton?.icon
-                        : ''
-                    }
-                </span>
-                <div className={(props.floating && isVisible ? '' : 'hidden ') + 'xl:block w-full ml-4 font-semibold'}>{props.sidebarActionButton?.label}</div>
+                <span className="material-symbols-rounded">{props.sidebarActionButton?.icon}</span>
+                <div className={labelStyle}>{props.sidebarActionButton?.label}</div>
             </Link>
         </li>
     );
