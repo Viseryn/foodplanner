414cb2b7e47f43064166c22b463b7008e11392dd
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-01-09 07:20:45 +0100

    Added a first version of the new color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,41 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'bg': '#fdfdf5',
+      'bg-dark': '#1a1c18',
+      'primary': {
+        100: '#2a6c00',
+        200: '#1e5200',
+      },
+      'primary-dark': {
+        100: '#8fda66',
+        200: '#1e5200',
+      },
+      'secondary': {
+        100: '#f2f6e9',
+        150: '#e7f6d8',
+        200: '#d9e7cb',
+        300: '#bdcbaf',
+        900: '#131f0d',
+      },
+      'secondary-dark': {
+        100: '#20261c',
+        200: '#3e4a35',
+        300: '#8d9286',
+        900: '#d9e7cb',
+      },
+      'tertiary': {
+        100: '#bbebeb',
+        200: '#a0cfcf',
+        900: '#002020',
+      },
+      'tertiary-dark': {
+        100: '#1e4e4e',
+        200: '#003737',
+        900: '#bbebeb',
+      },
+    }
   },
   plugins: [
     require('flowbite/plugin')
