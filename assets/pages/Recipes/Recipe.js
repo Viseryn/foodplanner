b36c82a5e23b5eacbb80a377f39bc60067f59914
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-01-09 18:16:26 +0100

    Update Recipe.js
    Added more comments, todos and fixed some margins

--- assets/pages/Recipes/Recipe.js
+++ assets/pages/Recipes/Recipe.js
@@ -191,6 +191,8 @@ export default function Recipe(props) {
 
     /**
      * Render
+     * 
+     * @todo Update skeletons!
      */
     return (
         <div className="">
@@ -238,125 +240,127 @@ export default function Recipe(props) {
                 }
             </div>
 
-
-{/* Ingredients, instructions and buttons */}
-{props.isLoadingRecipes || props.recipeIndex < 0
-    // ? <Card>
-    //     <TextParagraph />
-    // </Card>
-    ? <Spinner />
-    : <>
-        {recipe?.ingredients?.length > 0 &&
-            <>
-                <div className="mb-4 mx-6 md:ml-2 lg:ml-6 mt-10">
-                    <SecondHeading>
-                        Zutaten für 
-                            <select
-                                className="dark:placeholder-gray-400 dark:bg-[#323a41] border border-gray-400 dark:border-none rounded-full h-10 w-20 mx-4 px-6 shadow-sm dark:shadow-md transition duration-300 focus:border-blue-600"
-                                value={portionSize}
-                                onChange={e => setPortionSize(e.target.value)}
-                            > {/** @todo Colors for select widget! */}
-                                {[...Array(10)].map((x, index) => 
-                                    <option
-                                        key={index + 1}
-                                        value={index + 1}
-                                    >
-                                        {index + 1}
-                                    </option>
-                                )}
-                            </select>
-                            {portionSize == 1 ? 'Portion' : 'Portionen'}
-                    </SecondHeading>
-                </div>
-                <Card style="mx-4 md:ml-0 lg:ml-4">
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-x-6">
-                        {tmpRecipe?.ingredients.map(ingredient =>
-                            <div key={ingredient.id} className="flex items-center justify-between">
-                                <span>
-                                    {generateDisplayName(floatToFraction(ingredient.quantity_value), ingredient.quantity_unit, ingredient.name)}
-                                </span>
-
-                                <div className="flex flex-row">
-                                    <Button 
-                                        style={'shoppinglist-' + ingredient.id} 
-                                        onClick={() => { 
-                                            handleAddSingleToShoppingList(ingredient); 
-                                            document.getElementsByClassName('shoppinglist-' + ingredient.id)[0].firstChild.innerHTML = "done"; 
-                                        }}
-                                        icon="add_shopping_cart"
-                                        outlined={true}
-                                        role="tertiary"
-                                    />
-                                    {props.settings.showPantry &&
-                                        <Button
-                                            style={'pantry-' + ingredient.id} 
-                                            onClick={() => { 
-                                                handleAddSingleToPantry(ingredient); 
-                                                document.getElementsByClassName('pantry-' + ingredient.id)[0].firstChild.innerHTML = "done"; 
-                                            }}
-                                            icon="add_home"
-                                            outlined={true}
-                                            role="tertiary"
-                                        />
-                                    }
-                                </div>
+            {/* Ingredients, instructions and buttons */}
+            {props.isLoadingRecipes || props.recipeIndex < 0
+                // ? <Card>
+                //     <TextParagraph />
+                // </Card>
+                ? <Spinner />
+                : <>
+                    {/* Ingredients */}
+                    {recipe?.ingredients?.length > 0 &&
+                        <>
+                            <div className="mb-4 mx-6 md:ml-2 lg:ml-6 mt-10">
+                                <SecondHeading>
+                                    Zutaten für 
+                                        <select
+                                            className="dark:placeholder-gray-400 dark:bg-[#323a41] border border-gray-400 dark:border-none rounded-full h-10 w-20 mx-4 px-6 shadow-sm dark:shadow-md transition duration-300 focus:border-blue-600"
+                                            value={portionSize}
+                                            onChange={e => setPortionSize(e.target.value)}
+                                        > {/** @todo Colors for select widget! */}
+                                            {[...Array(10)].map((x, index) => 
+                                                <option
+                                                    key={index + 1}
+                                                    value={index + 1}
+                                                >
+                                                    {index + 1}
+                                                </option>
+                                            )}
+                                        </select>
+                                        {portionSize == 1 ? 'Portion' : 'Portionen'}
+                                </SecondHeading>
                             </div>
-                        )}
-                    </div>
+                            <Card style="mx-4 md:ml-0 lg:ml-4">
+                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-x-6">
+                                    {tmpRecipe?.ingredients.map(ingredient =>
+                                        <div key={ingredient.id} className="flex items-center justify-between">
+                                            <span>
+                                                {generateDisplayName(floatToFraction(ingredient.quantity_value), ingredient.quantity_unit, ingredient.name)}
+                                            </span>
 
-                {props.settings.showPantry &&
-                    <div className="flex justify-end mt-4">
+                                            <div className="flex flex-row">
+                                                <Button 
+                                                    style={'shoppinglist-' + ingredient.id} 
+                                                    onClick={() => { 
+                                                        handleAddSingleToShoppingList(ingredient); 
+                                                        document.getElementsByClassName('shoppinglist-' + ingredient.id)[0].firstChild.innerHTML = "done"; 
+                                                    }}
+                                                    icon="add_shopping_cart"
+                                                    outlined={true}
+                                                    role="tertiary"
+                                                />
+                                                {props.settings.showPantry &&
+                                                    <Button
+                                                        style={'pantry-' + ingredient.id} 
+                                                        onClick={() => { 
+                                                            handleAddSingleToPantry(ingredient); 
+                                                            document.getElementsByClassName('pantry-' + ingredient.id)[0].firstChild.innerHTML = "done"; 
+                                                        }}
+                                                        icon="add_home"
+                                                        outlined={true}
+                                                        role="tertiary"
+                                                    />
+                                                }
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+
+                            {props.settings.showPantry &&
+                                <div className="flex justify-end mt-4">
+                                    <Button
+                                        icon={showPantryButton ? 'add_home' : 'done'}
+                                        outlined={true}
+                                        label={showPantryButton ? 'Alle Zutaten zum Vorrat' : 'Erledigt!'}
+                                        onClick={() => handleAddPantry(tmpRecipe)}
+                                        role="secondary"
+                                        small={true}
+                                    />
+                                </div>
+                            }
+                            </Card>
+                        </>
+                    }
+
+                    {/* Instructions */}
+                    {recipe?.instructions?.length > 0 &&
+                        <div className="mt-10">
+                            <div className="mb-4 mx-6 md:ml-2 lg:ml-6">
+                                <SecondHeading>
+                                    Zubereitung
+                                </SecondHeading>
+                            </div>
+                            <Card style="space-y-2 mx-4 md:ml-0 lg:ml-4">
+                                {recipe?.instructions.map((instruction, index) =>
+                                    <div key={instruction.id} className="flex -px-6">
+                                        <span className="mr-2">{index + 1}.</span>
+                                        {instruction.instruction}
+                                    </div>
+                                )}
+                            </Card>
+                        </div>
+                    }
+
+                    {/* Show empty card if there is no image, no ingredients and no instructions */}
+                    {recipe?.image === undefined && recipe?.ingredients?.length === 0 && recipe?.instructions?.length === 0 &&
+                        <Card style="mx-4 md:ml-0 lg:ml-4">
+                            Hier gibt es noch nichts zu sehen.
+                        </Card>
+                    }
+
+                    {/* Edit Button */}
+                    <div className="flex justify-end px-4 pt-6">
                         <Button
-                            icon={showPantryButton ? 'add_home' : 'done'}
+                            location={`/recipe/${recipe?.id}/edit`}
+                            icon="edit"
                             outlined={true}
-                            label={showPantryButton ? 'Alle Zutaten zum Vorrat' : 'Erledigt!'}
-                            onClick={() => handleAddPantry(tmpRecipe)}
-                            role="secondary"
+                            label="Rezept bearbeiten"
+                            role="tertiary"
                             small={true}
                         />
                     </div>
-                }
-                </Card>
-            </>
-        }
-
-        {recipe?.instructions?.length > 0 &&
-            <div className="mt-10">
-                <div className="mb-4 mx-6 md:ml-2 lg:ml-6">
-                    <SecondHeading>
-                        Zubereitung
-                    </SecondHeading>
-                </div>
-                <Card style="space-y-2 mx-4 md:ml-0 lg:ml-4">
-                    {recipe?.instructions.map((instruction, index) =>
-                        <div key={instruction.id} className="flex -px-6">
-                            <span className="mr-2">{index + 1}.</span>
-                            {instruction.instruction}
-                        </div>
-                    )}
-                </Card>
-            </div>
-        }
-
-        {recipe?.image === undefined && recipe?.ingredients?.length === 0 && recipe?.instructions?.length === 0 &&
-            <div className="text-gray-400 mb-6">
-                Hier gibt es noch nichts zu sehen.
-            </div>
-        }
-
-        <div className="flex justify-end px-4 pt-6">
-            <Button
-                location={`/recipe/${recipe?.id}/edit`}
-                icon="edit"
-                outlined={true}
-                label="Rezept bearbeiten"
-                role="tertiary"
-                small={true}
-            />
-        </div>
-    </>
-}
-
+                </>
+            }
         </div>
     );
 }
