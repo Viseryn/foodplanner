2bd00d5020838997ca53aebba509e54bccbbcffc
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-01-09 23:09:49 +0100

    Update Registration.js

--- assets/pages/Registration/Registration.js
+++ assets/pages/Registration/Registration.js
@@ -11,6 +11,8 @@ import Button from '../../components/ui/Buttons/Button';
 import Heading from '../../components/ui/Heading';
 import Notification from '../../components/ui/Notification';
 import Spinner from '../../components/ui/Spinner';
+import Card from '../../components/ui/Card';
+import Spacer from '../../components/ui/Spacer';
 
 /**
  * Registration
@@ -63,70 +65,79 @@ export default function Registration(props) {
      * Render
      */
     return (
-        <div className="px-6 pb-[6.5rem] pt-6 md:pb-6 md:my-6 md:mr-6 w-full min-h-screen md:min-h-fit bg-white dark:bg-[#29353f] md:rounded-3xl md:w-fit">
-            <Heading>Registrierung</Heading>
-            <div className="min-w-[400px]"></div>
+        <div className="pb-[6.5rem] md:pb-6 w-full md:w-[400px]">
+            <div className="pt-4 md:pt-9 px-4">
+                <Heading>Registrierung</Heading>
+            </div>
 
-            {isLoadingSubmit && success && 
-                <Notification color="green" title="Registrierung erfolgreich!">
-                    Du kannst dich nun <Link to="/login">einloggen</Link>.
-                </Notification>
-            }
-            
-            {(props.isLoadingUser || isLoadingSubmit) && !success &&
-                <Spinner />
-            }
+            <Spacer height="10" />
 
-            {props.user?.username !== undefined && !props.isLoadingUser && !isLoadingSubmit &&
-                <Navigate to="/login" />
-            }
+            <div className="">
+                {isLoadingSubmit && success && 
+                    <Notification color="green" title="Registrierung erfolgreich!">
+                        Du kannst dich nun <Link to="/login">einloggen</Link>.
+                    </Notification>
+                }
+                
+                {(props.isLoadingUser || isLoadingSubmit) && !success &&
+                    <Spinner />
+                }
 
-            {props.user?.username === undefined && !props.isLoadingUser && !isLoadingSubmit &&
-                <div className="sm:w-[400px]">
+                {props.user?.username !== undefined && !props.isLoadingUser && !isLoadingSubmit &&
+                    <Navigate to="/login" />
+                }
+
+                {props.user?.username === undefined && !props.isLoadingUser && !isLoadingSubmit &&
                     <form onSubmit={handleSubmit} autoComplete="off">
-                        <InputRow 
-                            id="registration_form_username"
-                            label="Dein Benutzername"
-                            inputProps={{
-                                required: 'required', 
-                            }}
-                        />
-                        <InputRow 
-                            id="registration_form_plainPassword"
-                            label="Dein Passwort"
-                            inputProps={{
-                                required: 'required', 
-                                type: 'password',
-                            }}
-                        />
-                        <div className="mb-6 flex items-center">
-                            <input 
-                                id="registration_form_agreeTerms"
-                                name="registration_form[agreeTerms]"
-                                type="checkbox" 
-                                required="required"
-                                className="w-4 h-4 text-blue-600 bg-gray-100 rounded-full border-gray-300 dark:bg-gray-700 dark:border-gray-600 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 peer mr-4"
+                        <Card style="mx-4 md:mx-0">
+                            <InputRow 
+                                id="registration_form_username"
+                                label="Dein Benutzername"
+                                inputProps={{
+                                    required: 'required', 
+                                }}
                             />
-                            <label htmlFor="registration_form_agreeTerms">Ich stimme den Allgemeinen Nutzungsbedingungen und den Datenschutzbestimmungen zu.</label>
-                        </div>
+                            <InputRow 
+                                id="registration_form_plainPassword"
+                                label="Dein Passwort"
+                                inputProps={{
+                                    required: 'required', 
+                                    type: 'password',
+                                }}
+                            />
+                            <div className="mb-6 flex items-center">
+                                <input 
+                                    id="registration_form_agreeTerms"
+                                    name="registration_form[agreeTerms]"
+                                    type="checkbox" 
+                                    required="required"
+                                    className="w-4 h-4 mr-4 text-primary-100 bg-[#e0e4d6] rounded-sm border-2 border-[#c3c8bb] dark:bg-[#43483e] dark:border-[#8d9286] focus:ring-primary-100 focus:ring-2 peer"
+                                />
+                                <label htmlFor="registration_form_agreeTerms">Ich stimme den Nutzungsbedingungen und den Datenschutzbestimmungen zu.</label>
+                            </div>
 
-                        <p className="my-6 text-justify text-gray-400">
-                            Nach der Registrierung muss deinem Account manuell
-                            der Admin-Status verliehen werden, ehe du FoodPlanner
-                            nutzen kannst.
-                        </p>
+                            <hr className="border-t-secondary-200 dark:border-t-secondary-dark-200" />
 
-                        <div className="flex justify-end">
+                            <p className="mt-6 text-justify">
+                                Nach der Registrierung muss deinem Account manuell
+                                der Admin-Status verliehen werden, ehe du FoodPlanner
+                                nutzen kannst.
+                            </p>
+                        </Card>
+
+                        <Spacer height="6" />
+
+                        <div className="flex justify-end mx-4 md:mx-0">
                             <Button
                                 type="submit"
                                 icon="login"
                                 label="Registrieren"
-                                elevated={true}
+                                role="secondary"
                             />
                         </div>
                     </form>
-                </div>
-            }
+                }
+            </div>
         </div>
     );
 }
