1ab861a44b36dad82bb60bc5568c73d517847111
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-01-10 01:28:03 +0100

    Update RecipeListSkeleton.js

--- assets/pages/Recipes/components/RecipeListSkeleton.js
+++ assets/pages/Recipes/components/RecipeListSkeleton.js
@@ -21,21 +21,21 @@ import React from "react";
             'grid grid-cols-1 gap-2 animate-pulse ' 
             + (!props.isTwoColumns && 'sm:grid-cols-3 md:grid-cols-3')
         }>
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400 dark:bg-gray-700" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/75 dark:bg-gray-800/75" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/50 dark:bg-gray-700/50" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/75 dark:bg-gray-800/75" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400 dark:bg-gray-700" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400 dark:bg-gray-700" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/75 dark:bg-gray-800/75" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/50 dark:bg-gray-700/50" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/75 dark:bg-gray-800/75" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400 dark:bg-gray-700" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400 dark:bg-gray-700" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/75 dark:bg-gray-800/75" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/50 dark:bg-gray-700/50" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400/75 dark:bg-gray-800/75" />
-            <div className="rounded-xl h-36 w-full object-cover bg-gray-400 dark:bg-gray-700" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500 dark:bg-notification-700" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/75 dark:bg-notification-700/75" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/50 dark:bg-notification-700/50" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/75 dark:bg-notification-700/75" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500 dark:bg-notification-700" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500 dark:bg-notification-700" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/75 dark:bg-notification-700/75" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/50 dark:bg-notification-700/50" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/75 dark:bg-notification-700/75" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500 dark:bg-notification-700" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500 dark:bg-notification-700" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/75 dark:bg-notification-700/75" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/50 dark:bg-notification-700/50" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500/75 dark:bg-notification-700/75" />
+            <div className="rounded-xl h-36 w-full object-cover bg-notification-500 dark:bg-notification-700" />
         </div>
     );
 }
