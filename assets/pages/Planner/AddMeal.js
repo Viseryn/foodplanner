da47c43c1dec1e88e98f3a3d000a8927c38ccc12
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-01-13 17:06:50 +0100

    Added topbar and fixed spacing

--- assets/pages/Planner/AddMeal.js
+++ assets/pages/Planner/AddMeal.js
@@ -67,107 +67,117 @@ export default function AddMeal(props) {
     };
 
     /**
-     * Load sidebar
+     * Load layout
      */
     useEffect(() => {
-        props.setSidebarActiveItem('planner');
-        props.setSidebarActionButton();
+        // Load sidebar
+        props.setSidebarActiveItem('planner')
+        props.setSidebarActionButton()
+
+        // Load Topbar
+        props.setTopbar({
+            title: 'Neue Mahlzeit',
+            showBackButton: true,
+            backButtonPath: '/planner',
+        })
 
         // Scroll to top
-        window.scrollTo(0, 0);
-    }, []);
+        window.scrollTo(0, 0)
+    }, [])
 
     /**
      * Render
      */
     return (
-        <div className="pb-[6.5rem] w-full md:w-[450px]">
+        <div className="pb-24 md:pb-4 w-full md:w-[450px]">
             {isSubmitted && <Navigate to={'/planner'} />}
 
-            <div className="p-4 md:px-0 md:pt-9 mb-6">
-                <HeadingAndBackButton location="/planner">Mahlzeit hinzufügen</HeadingAndBackButton>
-            </div>
+            <Spacer height="6" />
 
             {isLoadingSubmit ? (
                 <Spinner />
             ) : (
-                <form className="md:max-w-[450px]" onSubmit={handleSubmit}>
-                    <Card style="mb-4 md:mb-6 mx-4 md:mx-0">
-                        <InputLabel id="meal_day" label="Für welchen Tag?" />
-                        {props.isLoadingDays ? (
-                            <div role="status" className="animate-pulse">
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
-                            </div>
-                        ) : (
-                            <SelectWidget
-                                id="meal_day"
-                                options={props.days}
-                                defaultValue={id}
-                            />
-                        )}
+                <div className="mx-4 md:mx-0">
+                    <form onSubmit={handleSubmit}>
+                        <Card>
+                            <InputLabel id="meal_day" label="Für welchen Tag?" />
+                            {props.isLoadingDays ? (
+                                <div role="status" className="animate-pulse">
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
+                                </div>
+                            ) : (
+                                <SelectWidget
+                                    id="meal_day"
+                                    options={props.days}
+                                    defaultValue={id}
+                                />
+                            )}
 
-                        <Spacer height="6" />
+                            <Spacer height="6" />
 
-                        <InputLabel id="meal_userGroup" label="Wann ist die Mahlzeit?" />
-                        {props.isLoadingUserGroups ? (
-                            <div role="status" className="animate-pulse">
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
-                            </div>
-                        ) : (
-                            <RadioWidget
-                                id="meal_mealCategory" 
-                                options={props.mealCategories}
-                                required={true}
-                            />
-                        )}
-                    </Card>
+                            <InputLabel id="meal_userGroup" label="Wann ist die Mahlzeit?" />
+                            {props.isLoadingUserGroups ? (
+                                <div role="status" className="animate-pulse">
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
+                                </div>
+                            ) : (
+                                <RadioWidget
+                                    id="meal_mealCategory" 
+                                    options={props.mealCategories}
+                                    required={true}
+                                />
+                            )}
+                        </Card>
 
-                    <Card style="mb-4 md:mb-6 mx-4 md:mx-0">
-                        <InputLabel htmlFor="meal_recipe" label="Welches Rezept?" />
-                        {props.isLoadingRecipes ? (
-                            <div role="status" className="max-w-sm animate-pulse">
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
-                            </div>
-                        ) : (
-                            <SelectWidget
-                                id="meal_recipe"
-                                options={props.recipes}
-                                required="required"
-                            />
-                        )}
+                        <Spacer height="4" />
 
-                        <Spacer height="6" />
+                        <Card>
+                            <InputLabel htmlFor="meal_recipe" label="Welches Rezept?" />
+                            {props.isLoadingRecipes ? (
+                                <div role="status" className="max-w-sm animate-pulse">
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
+                                </div>
+                            ) : (
+                                <SelectWidget
+                                    id="meal_recipe"
+                                    options={props.recipes}
+                                    required="required"
+                                />
+                            )}
 
-                        <InputLabel id="meal_userGroup" label="Für wen ist die Mahlzeit?" />
-                        {props.isLoadingUserGroups ? (
-                            <div role="status" className="animate-pulse">
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
-                                <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
-                            </div>
-                        ) : (
-                            <RadioWidget
-                                id="meal_userGroup" 
-                                options={props.userGroups}
-                                required={true}
-                            />
-                        )}
-                    </Card>
+                            <Spacer height="6" />
 
-                    <div className="flex justify-end mr-4 md:mr-0">
-                        {!props.isLoadingDays && !props.isLoadingRecipes && !props.isLoadingUserGroups && !props.isLoadingMealCategories &&
-                            <Button
-                                type="submit"
-                                icon="save" 
-                                label="Speichern" 
-                                outlined={true}
-                                floating={true}
-                            />
-                        }
-                    </div> 
-                </form>
+                            <InputLabel id="meal_userGroup" label="Für wen ist die Mahlzeit?" />
+                            {props.isLoadingUserGroups ? (
+                                <div role="status" className="animate-pulse">
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-2/3 mb-2"></div>
+                                    <div className="h-4 bg-gray-200 dark:bg-gray-800 rounded-full w-3/4"></div>
+                                </div>
+                            ) : (
+                                <RadioWidget
+                                    id="meal_userGroup" 
+                                    options={props.userGroups}
+                                    required={true}
+                                />
+                            )}
+                        </Card>
+
+                        <div className="flex justify-end pb-[5.5rem] md:pb-0 md:pt-4">
+                            {!props.isLoadingDays && !props.isLoadingRecipes && !props.isLoadingUserGroups && !props.isLoadingMealCategories &&
+                                <Button
+                                    type="submit"
+                                    icon="save" 
+                                    label="Speichern" 
+                                    outlined={true}
+                                    floating={true}
+                                />
+                            }
+                        </div> 
+                    </form>
+                </div>
             )}
         </div>
     );
