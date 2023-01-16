22e6623f17a95948c51ef9be5f82baa6314919d2
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-01-16 14:27:58 +0100

    Create SidebarDrawer.js

---
+++ assets/layouts/Sidebar/SidebarDrawer.js
@@ -0,0 +1,112 @@
+/*********************************************
+ * ./assets/layouts/Sidebar/SidebarDrawer.js *
+ *********************************************/
+
+import React                from 'react'
+
+import SidebarDrawerButton  from './components/SidebarDrawerButton'
+import SidebarDrawerItem    from './components/SidebarDrawerItem'
+
+/**
+ * SidebarDrawer
+ * 
+ * A layout component that renders the sidebar drawer.
+ * The sidebar drawer is controlled by the state variable
+ * isDrawerVisible from the parent component. When this 
+ * variable is set to true, the sidebar drawer will move 
+ * into the display area together with some semi-transparent
+ * overlay for the rest of the screen. 
+ * 
+ * The sidebar drawer has a SidebarDrawerButton that can 
+ * close the drawer again, and contains several links to 
+ * less important pages (or even external websites). Lastly,
+ * the drawer also shows the current version number of the app.
+ * 
+ * @component
+ * @property {boolean} isDrawerVisible A state variable that describes whether or not the sidebar drawer is visible.
+ * @property {React.Dispatch<ReactSetStateAction<boolean>>} setDrawerVisible The setter method of isDrawerVisible.
+ * @property {string} version The current version number of the app.
+ * @property {object} user A user object.
+ */
+export default function SidebarDrawer({ isDrawerVisible, setDrawerVisible, ...props }) {
+    return (
+        <>
+            {/* SidebarDrawer */}
+            <aside className={
+                'z-[9000] fixed h-full ease-in-out duration-300' 
+                + (isDrawerVisible ? '' : ' -translate-x-full')
+            }>
+                <div className="flex flex-col justify-start bg-bg dark:bg-bg-dark h-full w-80 p-2 md:p-6 pb-3">
+                    <ul className="mb-2 w-fit">
+                        <SidebarDrawerButton
+                            isDrawerVisible={isDrawerVisible}
+                            setDrawerVisible={setDrawerVisible} 
+                            icon="close"
+                        />
+                    </ul>
+
+                    <ul className="flex flex-col space-y-2 px-4 md:px-0">
+                        {props.user.data?.username === undefined ? (
+                            <>
+                                <SidebarDrawerItem 
+                                    id="login"
+                                    icon="login"
+                                    label="Einloggen"
+                                />
+                                <SidebarDrawerItem 
+                                    id="register"
+                                    icon="person_add"
+                                    label="Registrieren"
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <SidebarDrawerItem 
+                                    id="logout"
+                                    icon="logout"
+                                    label="Ausloggen"
+                                />
+                                <SidebarDrawerItem 
+                                    id="settings"
+                                    icon="settings"
+                                    label="Einstellungen"
+                                />
+                            </>
+                        )}
+                    </ul>
+
+                    <hr className="m-4 md:mx-0 border-t-secondary-dark-300" />
+
+                    <ul className="flex flex-col space-y-2 px-4 md:px-0">
+                        <SidebarDrawerItem 
+                            path="https://github.com/Viseryn/foodplanner"
+                            id="github"
+                            icon="developer_mode"
+                            label="GitHub"
+                        />
+                        <SidebarDrawerItem 
+                            path="https://yusel.net/blog"
+                            id="blog"
+                            icon="web"
+                            label="Blog"
+                        />
+                    </ul>
+
+                    {/* Current version */}
+                    <div className="text-xs flex justify-end absolute bottom-5 right-5">
+                        {props.version}
+                    </div>
+                </div>
+            </aside>
+                
+            {/* Background gradient for sidebar drawer */}
+            <div 
+                onClick={() => setDrawerVisible(!isDrawerVisible)} 
+                className={
+                    'bg-gradient-to-r from-black/75 duration-300 h-full w-full fixed z-[8000]'
+                    + (isDrawerVisible ? '' : ' opacity-0 -translate-x-full')
+                } 
+            />
+        </>
+    )
+}
