2db8dd137c22ea984b94c21cd26934fff4b99aa2
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-02 10:03:08 +0100

    Installed TypeScript and updated dependencies

---
+++ assets/app.tsx
@@ -0,0 +1,21 @@
+/**
+ * app.tsx
+ * 
+ * @author Kevin Sporbeck
+ * @version v1.3
+ */
+
+// Import CSS
+import './styles/app.scss'
+
+// Import SweetAlert
+import swal from 'sweetalert'
+
+// Import React
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './layouts/App'
+
+// Render app
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(<App version="v1.3" />)
