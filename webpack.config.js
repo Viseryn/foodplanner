2dea4bfc1a6c98ec92795659721aafdf9f86b863
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-02 12:01:51 +0100

    Added path alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -72,6 +73,10 @@ Encore
     //.autoProvidejQuery()
 
     .enablePostCssLoader()
+
+    .addAliases({
+        '@': path.resolve(__dirname, 'assets')
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
