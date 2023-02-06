7a2e8d65caff4a99e4e557e1b91f30d99969343f
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-06 14:15:07 +0100

    Updated documentation and type annotations

--- assets/hooks/useAuthentication.ts
+++ assets/hooks/useAuthentication.ts
@@ -9,41 +9,33 @@ import useFetch                 from './useFetch'
 /**
  * useAuthentication
  * 
- * Returns the current state of the user data state variable
- * and the authentication, especially whether the user is 
- * authenticated at the moment and whether the authentication
- * is done loading.
+ * Returns the current state of the user data state variable and the authentication, 
+ * especially whether the user is authenticated at the moment and whether the 
+ * authentication is done loading.
  * 
- * @returns An array of a user object and an authentication object.
+ * @returns An array of a User object and an Authentication object.
  */
 function useAuthentication(): [FetchableEntity<User>, Authentication] {
-    /**
-     * Whether the user is currently authenticated.
-     */
-    const [isAuthenticated, setAuthenticated] = useState(false)
+    // Whether the authentication process is still loading. This will only be 
+    // false after the user data has been loaded and isAuthenticated has been set 
+    // for the last time.
+    const [isLoading, setLoading] = useState<boolean>(true)
 
-    /**
-     * Whether the authentication process is still loading.
-     * This will only be false after the user data has been 
-     * loaded and isAuthenticated has been set for the last 
-     * time.
-     */
-    const [isLoading, setLoading] = useState(true)
+    // Whether the user is currently authenticated.
+    const [isAuthenticated, setAuthenticated] = useState<boolean>(false)
 
-    // User object
-    const user = useFetch<User>('/api/user')
-
-    // Authentication object
-    const authentication = { isAuthenticated, isLoading }
+    // User and Authentication objects
+    const user: FetchableEntity<User> = useFetch<User>('/api/user')
+    const authentication: Authentication = { isAuthenticated, isLoading }
 
     // Each time the user data changes, check if authenticated
     useEffect(() => {
-        const authenticated = user.data?.roles?.includes('ROLE_ADMIN') ?? false
+        const authenticated: boolean = user.data?.roles?.includes('ROLE_ADMIN') ?? false
+
         setAuthenticated(authenticated)
         setLoading(true)
 
-        // The loading of the authentication process will
-        // only be stopped once the user data has loaded
+        // The loading of the authentication will only be stopped once the user data is loaded
         if (!user.isLoading) {
             setLoading(false)
         }
