bf7ce6f5767f1554e6cdd33f320f19cf52380c2b
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-22 16:38:58 +0100

    Updated documentation and spacing

--- assets/layouts/App.tsx
+++ assets/layouts/App.tsx
@@ -142,16 +142,12 @@ export default function App({ version }: {
     
 
 
-    /******************
-     * RENDERING      *
-     ******************/
+    const props = {
+        user, authentication, settings, userGroups, mealCategories,
+        recipes, days, shoppingList, pantry, setSidebar, setTopbar
+    }
 
-    const props = { user, authentication, settings, userGroups, mealCategories, 
-        recipes, days, shoppingList, pantry, setSidebar, setTopbar }
-
-    /** 
-     * Render App
-     */
+    // Render App component
     return (
         <BrowserRouter>
             <div className="flex flex-col md:flex-row items-start bg-bg dark:bg-bg-dark min-h-screen text-secondary-900 dark:text-secondary-dark-900 min-w-[375px]">
