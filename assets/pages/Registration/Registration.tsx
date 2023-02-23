f271ef1c163a78186e69691b289d8a6fccae3ae3
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-23 18:41:54 +0100

    Updated to TypeScript and  cleanup

--- assets/pages/Registration/Registration.tsx
+++ assets/pages/Registration/Registration.tsx
@@ -1,17 +1,17 @@
-/***********************************************
- * ./assets/pages/Registration/Registration.js *
- ***********************************************/
+/************************************************
+ * ./assets/pages/Registration/Registration.tsx *
+ ************************************************/
 
 import React, { useEffect, useState } from 'react'
 import { Link, Navigate } from 'react-router-dom'
 import axios from 'axios'
 
-import { InputRow } from '../../components/form/Input'
-import Button       from '../../components/ui/Buttons/Button'
-import Card         from '../../components/ui/Card'
-import Notification from '../../components/ui/Notification'
-import Spacer       from '../../components/ui/Spacer'
-import Spinner      from '../../components/ui/Spinner'
+import { InputRow } from '@/components/form/Input'
+import Button from '@/components/ui/Buttons/Button'
+import Card from '@/components/ui/Card'
+import Notification from '@/components/ui/Notification'
+import Spacer from '@/components/ui/Spacer'
+import Spinner from '@/components/ui/Spinner'
 
 /**
  * Registration
@@ -19,25 +19,39 @@ import Spinner      from '../../components/ui/Spinner'
  * A component that renders a registration form.
  * 
  * @component
- * @param props
- * @param props.user
- * @param props.setSidebar
- * @param props.setTopbar
  */
-    /**
-     * State variables
-     */
 export default function Registration({ user, setSidebar, setTopbar }: {
     user: FetchableEntity<User>
     setSidebar: SetSidebarAction
     setTopbar: SetTopbarAction
 }): JSX.Element {
-    const [isLoadingSubmit, setLoadingSubmit] = useState(false)
-    const [success, setSuccess] = useState(false)
+    // State variables
+    const [isLoadingSubmit, setLoadingSubmit] = useState<boolean>(false)
+    const [success, setSuccess] = useState<boolean>(false)
 
     /**
-     * Load layout
+     * On submitting the form, calls the Register API.
+     * 
+     * @param event A submit form event.
      */
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+
+        setLoadingSubmit(true);
+
+        // Try API call
+        (async () => {
+            try {
+                await axios.post('/api/register', formData)
+                setSuccess(true)
+            } catch (error) {
+                console.log(error)
+            }
+        })()
+    }
+
+    // Load layout
     useEffect(() => {
         setSidebar()
         setTopbar({
@@ -45,29 +59,7 @@ export default function Registration({ user, setSidebar, setTopbar }: {
         })
     }, [])
 
-    /**
-     * handleSubmit
-     * 
-     * On submitting the form, calls the Register API.
-     * 
-     * @param {*} event
-     */
-    const handleSubmit = (event) => {
-        const formData = new FormData(event.target)
-        event.preventDefault()
-
-        setLoadingSubmit(true)
-
-        axios
-            .post('/api/register', formData)
-            .then(() => {
-                setSuccess(true)
-            })
-    }
-
-    /**
-     * Render
-     */
+    // Render Registration
     return (
         <div className="pb-24 md:pb-4 md:w-[400px]">
             <Spacer height="6" />
@@ -112,18 +104,18 @@ export default function Registration({ user, setSidebar, setTopbar }: {
                                     id="registration_form_agreeTerms"
                                     name="registration_form[agreeTerms]"
                                     type="checkbox" 
-                                    required="required"
+                                    required={true}
                                     className="w-4 h-4 mr-4 text-primary-100 bg-[#e0e4d6] rounded-sm border border-[#c3c8bb] dark:bg-[#43483e] dark:border-[#8d9286] focus:ring-primary-100 focus:ring-2 peer"
                                 />
-                                <label htmlFor="registration_form_agreeTerms">Ich stimme den Nutzungsbedingungen und den Datenschutzbestimmungen zu.</label>
+                                <label htmlFor="registration_form_agreeTerms">
+                                    Ich stimme den Nutzungsbedingungen und den Datenschutzbestimmungen zu.
+                                </label>
                             </div>
 
                             <hr className="my-6 border-t-secondary-200 dark:border-t-secondary-dark-200" />
 
                             <p className="text-justify">
-                                Nach der Registrierung muss deinem Account manuell
-                                der Admin-Status verliehen werden, ehe du FoodPlanner
-                                nutzen kannst.
+                                Nach der Registrierung muss deinem Account manuell der Admin-Status verliehen werden, ehe du FoodPlanner nutzen kannst.
                             </p>
                         </Card>
 
