17f1b15daadbbd7de6ccb147dd01a411bcd8f1b1
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-24 14:50:47 +0100

    Updated Notification to TypeScript

--- assets/components/ui/Notification.tsx
+++ assets/components/ui/Notification.tsx
@@ -2,7 +2,7 @@
  * ./assets/components/ui/Notification.tsx *
  *******************************************/
 
-import React from 'react';
+import React from 'react'
 
 /**
  * Notification
@@ -11,43 +11,47 @@ import React from 'react';
  * containing a message. 
  * 
  * @component
- * @property {string} title The title of the notification box.
- * @property {?string} color The color of the notification box. Default is gray.
- * @property {?string} icon The material-symbols-rounded identifier for the icon. Default is 'info'.
- * @property {*} children Any content under the title. Can be empty.
+ * @param props
+ * @param props.title The title of the notification box.
+ * @param props.color The color of the notification box. Default is gray.
+ * @param props.icon The Material Symbols identifier for the icon. Default is 'info'.
+ * @param props.children The DOM children of the Notification component.
  * 
  * @example
  * <Notification color="red" icon="info" title="Error 404">
  *     The page you requested could not be found.
  * </Notification>
  */
-export default function Notification(props) {
-    let notificationStyle = 'p-6 space-x-4 text-sm rounded-xl w-full';
+export default function Notification({ title, color = '', icon = 'info', children }: {
+    title?: string
+    color?: string
+    icon?: string
+    children: React.ReactNode
+}): JSX.Element {
+    let notificationStyle: string = 'p-6 space-x-4 text-sm rounded-xl w-full'
 
-    if (props.color === 'red') {
-        notificationStyle += ' text-notification-200 dark:text-notification-400 bg-notification-100 dark:bg-notification-300';
-    } else if(props.color === 'green') {
-        notificationStyle += ' text-tertiary-900 dark:text-tertiary-dark-900 bg-tertiary-100 dark:bg-tertiary-dark-100';
+    if (color === 'red') {
+        notificationStyle += ' text-notification-200 dark:text-notification-400 bg-notification-100 dark:bg-notification-300'
+    } else if(color === 'green') {
+        notificationStyle += ' text-tertiary-900 dark:text-tertiary-dark-900 bg-tertiary-100 dark:bg-tertiary-dark-100'
     } else {
-        notificationStyle += ' text-notification-600 dark:text-notification-800 bg-notification-500 dark:bg-notification-700';
+        notificationStyle += ' text-notification-600 dark:text-notification-800 bg-notification-500 dark:bg-notification-700'
     }
 
-    return (
-        <div className={notificationStyle}>
-            <div className="flex items-center">
-                <span className="material-symbols-rounded mr-6">
-                    {props.icon || 'info'}
-                </span>
-                <div>
-                    <div className="font-semibold">{props.title}</div>
+    return <div className={notificationStyle}>
+        <div className="flex items-center">
+            <span className="material-symbols-rounded mr-6">
+                {icon}
+            </span>
+            <div>
+                <div className="font-semibold">{title}</div>
 
-                    {props?.children !== undefined &&
-                        <div className={props?.title !== undefined ? 'mt-2' : ''}>
-                            {props.children}
-                        </div>
-                    }
-                </div>
+                {children !== undefined &&
+                    <div className={title !== undefined ? 'mt-2' : ''}>
+                        {children}
+                    </div>
+                }
             </div>
         </div>
-    );
+    </div>
 }
