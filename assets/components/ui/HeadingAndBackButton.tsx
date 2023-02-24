53ef7767595c96bc25a9894218b43944decaae63
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-24 14:44:19 +0100

    Updated HeadingAndBackButton to TypeScript

--- assets/components/ui/HeadingAndBackButton.tsx
+++ assets/components/ui/HeadingAndBackButton.tsx
@@ -2,11 +2,11 @@
  * ./assets/components/ui/HeadingAndBackButton.tsx *
  ***************************************************/
 
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-import IconButton from './Buttons/IconButton';
-import Heading from './Heading';
+import Heading from './Heading'
+import IconButton from './Buttons/IconButton'
 
 /**
  * HeadingAndBackButton
@@ -14,24 +14,25 @@ import Heading from './Heading';
  * A component that renders a huge heading and a back button.
  * 
  * @component
- * @property {string} location The location that the back button navigates to.
- * @property {*} children 
+ * @param props
+ * @param props.location The location that the back button navigates to.
+ * @param props.children The DOM children of the Card component.
  * 
- * @example
- * <HeadingAndBackButton>This is a title</Heading>
+ * @example <HeadingAndBackButton location="/planner">This is a title</Heading>
  */
-export default function HeadingAndBackButton(props) {
-    return (
-        <div className="flex justify-start items-center">
-            <div className="flex justify-between">
-                <Link to={props.location} className="mr-4 text-primary-200 dark:text-secondary-dark-900">
-                    <IconButton>
-                        arrow_back
-                    </IconButton>
-                </Link>
-            </div>
-
-            <Heading>{props.children}</Heading>
+export default function HeadingAndBackButton({ location = '#', children }: {
+    location?: string
+    children: React.ReactNode
+}): JSX.Element {
+    return <div className="flex justify-start items-center">
+        <div className="flex justify-between">
+            <Link to={location} className="mr-4 text-primary-200 dark:text-secondary-dark-900">
+                <IconButton>
+                    arrow_back
+                </IconButton>
+            </Link>
         </div>
-    );
+
+        <Heading>{children}</Heading>
+    </div>
 }
