1c22113e68997cf04370fd19faf3bf5c37ce02ef
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-24 15:03:34 +0100

    Updated IconButton to TypeScript

--- assets/components/ui/Buttons/IconButton.tsx
+++ assets/components/ui/Buttons/IconButton.tsx
@@ -2,38 +2,36 @@
  * ./assets/components/ui/Buttons/IconButton.tsx *
  *************************************************/
 
-import React from 'react';
+import React from 'react'
 
 /**
  * IconButton
  * 
- * A component that renders a button with only an icon, for example for header navigation.
+ * A component that renders a button with only an icon, for example for the topbar.
  * 
  * @component
- * @property {?boolean} outlined Whether or not the icon should be outlines only (true) or filled (false, default).
- * @property {?string} style Additional styling classes.
- * @property {*} props An optional object of properties of the <span /> element, e.g. onClick handling.
+ * @param props
+ * @param props.outlined Whether or not the icon should be only outlines (true) or filled (false, by default).
+ * @param props.style Additional styling classes.
+ * @param props.onClick An optional onClick handler.
+ * @param props.children The Material Symbols identifier for the icon.
  * 
- * @example
- * <IconButton
- *     outlined={true}
- *     onClick={() => someHandler(params)}
- * >
- *     sync
- * </IconButton>
+ * @example <IconButton outlined={true} onClick={() => someHandler(params)}>sync</IconButton>
  */
-export default function IconButton({ outlined = false, style = '', ...props }) {
-    return (
-        <span 
-            className={
-                'material-symbols-rounded cursor-pointer transition duration-300 '
-                + 'hover:bg-secondary-200 dark:hover:bg-secondary-dark-200 p-2 rounded-full' 
-                + (outlined ? ' outlined' : '')
-                + ' ' + style
-            }
-            {...props} 
-        >
-            {props.children}
-        </span>
-    );
+export default function IconButton({ outlined = false, style = '', onClick, children }: {
+    outlined?: boolean
+    style?: string
+    onClick?: () => void
+    children: string
+}): JSX.Element {
+    return <span 
+        onClick={onClick}
+        className={
+            'material-symbols-rounded cursor-pointer transition duration-300 hover:bg-secondary-200 dark:hover:bg-secondary-dark-200 p-2 rounded-full' 
+            + (outlined ? ' outlined' : '')
+            + ' ' + style
+        }
+    >
+        {children}
+    </span>
 }
