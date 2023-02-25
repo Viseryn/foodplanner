a0cf8582aa6337780ccd71702902720108a6c84e
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-25 23:41:05 +0100

    Create SelectRow.tsx

---
+++ assets/components/form/Select/SelectRow.tsx
@@ -0,0 +1,34 @@
+/******************************************
+ * ./assets/components/form/SelectRow.tsx *
+ ******************************************/
+
+import React from 'react'
+import Label from '../Label'
+import rowStyle from '../util/rowStyle'
+import SelectWidget from './SelectWidget'
+
+/**
+ * SelectRow
+ * 
+ * A component that renders a select input field with a corresponding label inside a div container.
+ * 
+ * @component
+ * @param props
+ * @param props.id The id of the select input field.
+ * @param props.label The label of the select input field.
+ * @param props.options An array of objects that represent the select options.
+ * @param props.disabledOption Optional: The text of the disabled first option.
+ * @param props.style Optional alternative styling classes for the div container.
+ */
+export default function SelectRow({ id, label, options, disabledOption, style = rowStyle, ...widgetProps }: {
+    id: string
+    label: string
+    options?: Array<{ id: string, title?: string }>
+    disabledOption?: string
+    style?: string
+}): JSX.Element {
+    return <div className={style}>
+        <Label htmlFor={id}>{label}</Label>
+        <SelectWidget id={id} options={options} disabledOption={disabledOption} {...widgetProps} />
+    </div>
+}
