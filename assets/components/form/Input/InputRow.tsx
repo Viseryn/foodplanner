dd548dc270430fb522933b203dd521ee468291c2
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-25 01:19:14 +0100

    Create InputRow.tsx

---
+++ assets/components/form/Input/InputRow.tsx
@@ -0,0 +1,33 @@
+/**************************************************
+ * ./assets/components/form/Textarea/InputRow.tsx *
+ **************************************************/
+
+import React from 'react'
+import Label from '../Label'
+import rowStyle from '../util/rowStyle'
+import InputWidget from './InputWidget'
+
+/**
+ * InputRow
+ * 
+ * A component that renders an input field with a corresponding label inside a div container.
+ * 
+ * @component
+ * @param props
+ * @param props.id The id of the input field.
+ * @param props.label The label of the input field.
+ * @param props.type The type of the input field. Default is 'text'.
+ * @param props.style Optional alternative styling classes for the div container.
+ * 
+ */
+export default function InputRow({ id, label, type = 'text', style = rowStyle, ...widgetProps }: {
+    id: string
+    label: string
+    type?: string
+    style?: string
+}): JSX.Element {
+    return <div className={style}>
+        <Label htmlFor={id}>{label}</Label>
+        <InputWidget id={id} type={type} {...widgetProps} />
+    </div>
+}
