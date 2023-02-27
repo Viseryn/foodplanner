107129053a14fa250f9c8c2637f07676d4c65cf4
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-27 22:18:13 +0100

    Marked rowStyle as deprecated

--- assets/components/form/util/rowStyle.ts
+++ assets/components/form/util/rowStyle.ts
@@ -4,7 +4,9 @@
 
 /**
  * The default styling for a form widget row.
+ * 
+ * @deprecated Use a Spacer component between widget rows instead.
  */
-const rowStyle: string = 'mb-6'
+const rowStyle: string = ''//'mb-6'
 
 export default rowStyle
