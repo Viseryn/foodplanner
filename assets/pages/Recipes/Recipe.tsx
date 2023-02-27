e327098eea500493d67a3e7cb65c85d09f7ecf1c
Kevin <info@yusel.net>
2023-02-27 14:05:05 +0100

    Renamed Recipe.js to Recipe.tsx

--- assets/pages/Recipes/Recipe.tsx
+++ assets/pages/Recipes/Recipe.tsx
@@ -1,6 +1,6 @@
-/************************************
- * ./assets/pages/Recipes/Recipe.js *
- ************************************/
+/*************************************
+ * ./assets/pages/Recipes/Recipe.tsx *
+ *************************************/
 
 import React, { useEffect, useState }       from 'react'
 import { useNavigate, useParams }           from 'react-router-dom'
