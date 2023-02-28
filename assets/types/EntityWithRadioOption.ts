9a2aec0f1e68aa177b4c77af0c2a1871a87ef78c
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-28 16:21:17 +0100

    Create EntityWithRadioOption.ts

---
+++ assets/types/EntityWithRadioOption.ts
@@ -0,0 +1,14 @@
+/*******************************************
+ * ./assets/types/EntityWithRadioOption.ts *
+ *******************************************/
+
+import RadioOption from './RadioOption'
+
+/**
+ * Model for an entity that can be an option of a radio input widget.
+ */
+type EntityWithRadioOption = {
+    option: RadioOption
+}
+
+export default EntityWithRadioOption
