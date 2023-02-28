f56574d0deb8748c27a95d31daa5379259d07927
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-28 15:35:25 +0100

    Create InstructionModel.ts

---
+++ assets/types/InstructionModel.ts
@@ -0,0 +1,18 @@
+/**************************************
+ * ./assets/types/InstructionModel.ts *
+ **************************************/
+
+import RecipeModel from './RecipeModel'
+
+/**
+ * Type specifications for Instruction objects returned by APIs
+ */
+type InstructionModel = {
+    /** The entity id of the Instruction object. */
+    id: number
+
+    /** The text of the Instruction object. */
+    instruction: string
+}
+
+export default InstructionModel
