ff80d79b27774a2758d020587418251ae22c8191
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-28 17:18:59 +0100

    More entity classes implement EntityModel

--- src/Entity/MealCategory.php
+++ src/Entity/MealCategory.php
@@ -6,7 +6,7 @@ use App\Repository\MealCategoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MealCategoryRepository::class)]
-class MealCategory
+class MealCategory implements EntityModel
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
--- src/Entity/UserGroup.php
+++ src/Entity/UserGroup.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserGroupRepository::class)]
-class UserGroup
+class UserGroup implements EntityModel
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
