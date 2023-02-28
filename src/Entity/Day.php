ebe50de81eed289db563957d7bc933401c0a1b9b
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-28 17:02:01 +0100

    More entity classes implement EntityModel

--- src/Entity/Day.php
+++ src/Entity/Day.php
@@ -9,7 +9,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DayRepository::class)]
-class Day
+class Day implements EntityModel
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
