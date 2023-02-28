e0e35265ddf0b5470477670eed0cd95bfc20a0b9
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-02-28 15:35:19 +0100

    Create EntityWithSelectOption.ts

---
+++ assets/types/EntityWithSelectOption.ts
@@ -0,0 +1,14 @@
+/********************************************
+ * ./assets/types/EntityWithSelectOption.ts *
+ ********************************************/
+
+import SelectOption from './SelectOption'
+
+/**
+ * Model for an entity that can be an option of a select input widget.
+ */
+type EntityWithSelectOption = {
+    option: SelectOption
+}
+
+export default EntityWithSelectOption
