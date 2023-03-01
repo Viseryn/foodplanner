4cad77460ef6021837a0fbc34a020a45069cd94f
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-03-01 14:46:08 +0100

    Create getOptions.ts

---
+++ assets/util/getOptions.ts
@@ -0,0 +1,19 @@
+/*******************************
+ * ./assets/util/getOptions.ts *
+ *******************************/
+
+import EntityWithOption from '@/types/EntityWithOption'
+
+/**
+ * @template OptionType A type that describes an option, e.g. SelectOption or RadioOption.
+ * @template EntityModel An entity type that extends EntityWithOption<OptionType>.
+ * @param entities An array of entities of type EntityModel.
+ * @returns An array of the option properties of each entity.
+ * 
+ * @example getOptionsForEntities(recipes.data) = [recipes.data[0].option, ...]
+ */
+export default function getOptions<OptionType, EntityModel extends EntityWithOption<OptionType>>(
+    entities: Array<EntityModel>
+): Array<OptionType> {
+    return entities.map(entity => entity.option)
+}
