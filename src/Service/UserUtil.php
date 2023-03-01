1cc6a00f23974f356480104c0ec73a412a0b51ee
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-03-01 13:31:34 +0100

    Create UserUtil.php

---
+++ src/Service/UserUtil.php
@@ -0,0 +1,20 @@
+<?php namespace App\Service;
+
+use App\Entity\EntityModel;
+use App\Entity\User;
+
+/**
+ * UserUtil
+ */
+class UserUtil extends EntityUtil
+{
+    /** @param User $user */
+    public function getApiModel(EntityModel $user): array
+    {
+        return [
+            'id' => $user->getId(),
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+        ];
+    }
+}
