781702af5db51b66b3a121948f638ef7eaf66fee
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-03-01 13:30:20 +0100

    User class implements EntityModel now

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface, EntityModel
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
