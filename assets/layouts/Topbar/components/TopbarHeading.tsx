59927cf5b6956fc0221a79bff180479bbfe23633
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-03-02 18:05:22 +0100

    Renamed HeadingAndBackButton to TopbarHeading

--- assets/layouts/Topbar/components/TopbarHeading.tsx
+++ assets/layouts/Topbar/components/TopbarHeading.tsx
@@ -1,35 +1,29 @@
-/***************************************************
- * ./assets/components/ui/HeadingAndBackButton.tsx *
- ***************************************************/
+/********************************************
+ * ./assets/components/ui/TopbarHeading.tsx *
+ ********************************************/
 
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Heading from './Heading'
-import IconButton from './Buttons/IconButton'
+import IconButton from '@/components/ui/Buttons/IconButton'
+import Heading from '@/components/ui/Heading'
 
 /**
- * HeadingAndBackButton
- * 
  * A component that renders a huge heading and a back button.
  * 
  * @component
  * @param props
  * @param props.location The location that the back button navigates to.
  * @param props.children The DOM children of the Card component.
- * 
- * @example <HeadingAndBackButton location="/planner">This is a title</Heading>
  */
-export default function HeadingAndBackButton({ location = '#', children }: {
+export default function TopbarHeading({ location = '#', children }: {
     location?: string
-    children: React.ReactNode
+    children?: React.ReactNode
 }): JSX.Element {
     return <div className="flex justify-start items-center">
         <div className="flex justify-between">
             <Link to={location} className="mr-4 text-primary-200 dark:text-secondary-dark-900">
-                <IconButton>
-                    arrow_back
-                </IconButton>
+                <IconButton>arrow_back</IconButton>
             </Link>
         </div>
 
