a351ae62c954e428c418dd7118888546e524aa37
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-03-02 16:03:51 +0100

    Update UserController.php

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use App\Service\UserUtil;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,36 +11,49 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use JMS\Serializer\SerializerBuilder;
 
+/**
+ * User API
+ */
 #[Route('/api/user')]
 class UserController extends AbstractController
 {
     /**
-     * User API
+     * User Detail API
+     * 
+     * Responds with a JSON object matching the type specifications of UserModel.ts.
      *
      * @return Response
      */
-    #[Route('/', name: 'api_user')]
-    public function index(UserUtil $userUtil): Response
+    #[Route('/detail', name: 'api_user_detail')]
+    public function detail(UserUtil $userUtil): Response
     {
+        // For the User object we need a little workaround, since $this->getUser() returns a 
+        // UserInterface and not a User object (which is what we need). Hence, we use this little 
+        // helper function to cast the result of $this->getUser() to a User object. If the former 
+        // is null, we just return an empty User object. (In fact, it will have the ROLE_USER role,
+        // but a user needs ROLE_ADMIN to access anything.)
+        $castToUser = function(null|UserInterface|User $userParam): User {
+            return $userParam ?: new User;
+        };
+
         $serializer = SerializerBuilder::create()->build();
-        $jsonContent = $serializer->serialize($userUtil->getApiModel($this->getUser()), 'json');
+        $jsonContent = $serializer->serialize($userUtil->getApiModel($castToUser($this->getUser())), 'json');
 
         return new JsonResponse($jsonContent);
     }
 
     /**
      * User List API
+     * 
+     * Responds with an array of JSON object matching the type specifications of UserGroupModel.ts.
      */
     #[Route('/list', name: 'api_user_list')]
     public function users(UserRepository $userRepository, UserUtil $userUtil): Response
     {
-        // Deny access if not logged in
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
 
-        // Fetch all User objects from the database
         $users = $userRepository->findAll();
 
-        // Serialize data and respond
         $serializer = SerializerBuilder::create()->build();
         $jsonContent = $serializer->serialize($userUtil->getApiModels($users), 'json');
 
