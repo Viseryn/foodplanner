c40f86c62fb2dfe62c86fd5b61fc4ff6b42d833d
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-03-10 00:51:24 +0100

    Update Planner.tsx
    * Added a Carousel view for larger displays, using the Carousel component and the useMediaQuery hook.
    * The MealTile component is now used, since it appeared twice in the code with minimal difference.

--- assets/pages/Planner/Planner.tsx
+++ assets/pages/Planner/Planner.tsx
@@ -7,14 +7,19 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import swal from 'sweetalert'
 
+import Button from '@/components/ui/Buttons/Button'
 import Card from '@/components/ui/Card'
+import Carousel from '@/components/ui/Carousel/Carousel'
+import Heading from '@/components/ui/Heading'
 import Spacer from '@/components/ui/Spacer'
 import Spinner from '@/components/ui/Spinner'
+import useMediaQuery from '@/hooks/useMediaQuery'
 import DayModel from '@/types/DayModel'
 import IngredientModel from '@/types/IngredientModel'
 import MealModel from '@/types/MealModel'
 import RecipeModel from '@/types/RecipeModel'
 import getFullIngredientName from '@/util/getFullIngredientName'
+import MealTile from './components/MealTile'
 
 /**
  * A component that renders the Weekly Planner. Shows a list of ten Day entities which each have a 
@@ -36,6 +41,9 @@ export default function Planner({ days, recipes, shoppingList, setSidebar, setTo
 
     // Whether the SidebarActionButton should display "Done!" on click.
     const [showSabDone, setShowSabDone] = useState<boolean>(false)
+
+    // Current mediaQuery selector
+    const medium = useMediaQuery()
 
     /**
      * When called, opens a SweetAlert. If it is confirmed, then the Meal Delete API is called and 
@@ -148,63 +156,77 @@ export default function Planner({ days, recipes, shoppingList, setSidebar, setTo
         window.scrollTo(0, 0)
     }, [])
     
+    // Configure carousel
+    const gap: number = 5
+
+    // Map the number of columns to each media query selector
+    const mediaColumnMap: Map<string, number> = new Map()
+    mediaColumnMap.set('md', 3)
+    mediaColumnMap.set('lg', 5)
+    mediaColumnMap.set('xl', 5)
+
+    // Calculate the width of a container for the carousel
+    const calculateWidth = (visibleItems: number, gap: number): number => {
+        // Each column is 160px, each gap is 4*gap wide, padding of 16 on both ends
+        return visibleItems * 160 + (visibleItems - 1) * 4 * gap + 32
+    }
+
+    // Build styling classes for the width of the container for the carousel
+    const mediumStyle: Map<string, { width: string }> = new Map()
+    mediaColumnMap.forEach((columns, medium) => {
+        mediumStyle.set(medium, { width: `${calculateWidth(columns ?? 0, gap)}px` })
+    })
+
     // Render Planner component
-    return <div className="pb-24 md:pb-4 w-full md:w-fit md:min-w-[450px] md:max-w-[900px]">
+    return <div className={`pb-24 md:pb-4 w-full`} style={mediumStyle.get(medium)}>
         <Spacer height="6" />
 
         {days.isLoading ? (
             <Spinner /> /** @todo Add skeletons */
         ) : (
-            <div className="pb-[5.5rem] md:pb-0 mx-4 md:ml-0 flex flex-col gap-4">
-            {/* The container might be bigger than the screen (md+), so leave extra margin to the right. */}
+            mediaColumnMap.has(medium)
+            ? <Card>
+                <Carousel visibleItems={mediaColumnMap.get(medium)} gap={gap} translation={160 + gap * 4}>
+                    {days.data.map(day => 
+                        <div key={day.id} className="flex flex-col gap-2 w-40">
+                            <div className="flex justify-between items-center pb-2">
+                                <Heading size="lg" style="pl-2">
+                                    {day.weekday},<br />
+                                    {day.date}
+                                </Heading>
+
+                                <Button
+                                    location={`/planner/add/${day.id}`}
+                                    icon="add"
+                                    role="secondary"
+                                />
+                            </div>
+
+                            {day.meals.map(meal =>
+                                <MealTile key={meal.id} meal={meal} deleteMeal={deleteMeal} isSmall={true} />
+                            )}
+                        </div>
+                    )}
+                </Carousel>
+            </Card>
+            : <div className="pb-[5.5rem] mx-4 flex flex-col gap-4">
                 {days.data.map(day =>
                     <Card key={day.id}>
-                        <div className="text-xl font-semibold text-primary-200 dark:text-secondary-dark-900">
-                            <span>{day.weekday}, {day.date}</span>
-                        </div>
+                        <Heading size="xl">{day.weekday}, {day.date}</Heading>
 
                         <Spacer height="4" />
 
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                             {day.meals.map(meal =>
-                                <div key={meal.id} className="h-40 w-full rounded-2xl transition duration-300">
-                                    <div className="relative group">
-                                        <img 
-                                            className="rounded-2xl h-40 w-full object-cover brightness-[.7]" 
-                                            src={meal.recipe.image?.filename != null 
-                                                ? meal.recipe.image?.directory + meal.recipe.image?.filename
-                                                : '/img/default.jpg'
-                                            } 
-                                            alt={meal.recipe.title}
-                                        />
-                                        <Link 
-                                            to={'/recipe/' + meal.recipe.id} 
-                                            className="absolute w-full bottom-4 px-6 text-white font-semibold"
-                                        >
-                                            <div className="text-xl mr-4">{meal.recipe.title}</div>
-                                            <div className="text-md flex items-center mt-2">
-                                                <span className="material-symbols-rounded mr-2">{meal.userGroup.icon}</span>
-                                                <span>{meal.userGroup.name}</span>
-                                            </div>
-                                            <div className="text-md flex items-center mt-2">
-                                                <span className="material-symbols-rounded mr-2">{meal.mealCategory.icon}</span>
-                                                <span>{meal.mealCategory.name}</span>
-                                            </div>
-                                        </Link>
-                                        <span 
-                                            onClick={() => deleteMeal(meal)}
-                                            className="cursor-pointer transition duration-300 group-hover:block material-symbols-rounded text-white absolute top-2 right-2
-                                            hover:bg-gray-600/40 p-1 rounded-full"
-                                        >
-                                            close
-                                        </span>
-                                    </div>
-                                </div>
+                                <MealTile key={meal.id} meal={meal} deleteMeal={deleteMeal} />
                             )}
 
-                            <Link 
-                                to={'/planner/add/' + day.id} 
-                                className={(day.meals.length > 0 ? 'h-14 md:h-40' : 'h-40') + ' w-full rounded-2xl transition duration-300 text-primary-100 dark:text-primary-dark-100 bg-secondary-200 dark:bg-secondary-dark-200 hover:bg-secondary-300 dark:hover:bg-secondary-dark-300 font-semibold text-lg flex justify-center items-center flex-row md:flex-col gap-4'}
+                            <Link
+                                to={'/planner/add/' + day.id}
+                                className={
+                                    (day.meals.length > 0 ? ('h-14' + (day.meals.length % 2 === 0 ? ' sm:col-span-2' : ' sm:h-40') ) : 'h-40') 
+                                    + ' w-full rounded-2xl transition duration-300 text-primary-100 dark:text-primary-dark-100 bg-secondary-200 dark:bg-secondary-dark-200 hover:bg-secondary-300 dark:hover:bg-secondary-dark-300 font-semibold text-lg flex justify-center items-center flex-row md:flex-col gap-4'
+                                }
                             >
                                 <span className="material-symbols-rounded">add</span>
                                 <span className="mx-6">Neue Mahlzeit</span>
