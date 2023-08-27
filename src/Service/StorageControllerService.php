d7641f325329188a58dfe8f3611ef8675ecdbe49
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-08-27 20:58:40 +0200

    Refactor Recipes API: Added utilities for new POST API #2

--- src/Service/StorageControllerService.php
+++ src/Service/StorageControllerService.php
@@ -1,9 +1,7 @@
 <?php namespace App\Service;
 
-use App\Entity\Ingredient;
 use App\Entity\Storage;
 use App\Repository\IngredientRepository;
-use Doctrine\Common\Collections\ArrayCollection;
 
 final class StorageControllerService
 {
@@ -18,28 +16,5 @@ final class StorageControllerService
         foreach($ingredients as $ingredient) {
             $this->ingredientRepository->remove($ingredient, true);
         }
-    }
-
-    public function mapIngredientModelToEntity(object $ingredientModel): Ingredient
-    {
-        return (new Ingredient)->setName($ingredientModel->name)
-                               ->setQuantityValue($ingredientModel->quantityValue)
-                               ->setQuantityUnit($ingredientModel->quantityUnit)
-                               ->setPosition($ingredientModel->position)
-                               ->setChecked($ingredientModel->checked);
-    }
-
-    /**
-     * @param object[] $ingredientModels
-     * @return ArrayCollection<Ingredient>
-     */
-    public function mapIngredientModelsToEntities(array $ingredientModels): ArrayCollection
-    {
-        /** @var ArrayCollection<Ingredient> $ingredients */
-        $ingredients = new ArrayCollection();
-        foreach ($ingredientModels as $ingredientModel) {
-            $ingredients->add($this->mapIngredientModelToEntity($ingredientModel));
-        }
-        return $ingredients;
     }
 }
