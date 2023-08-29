392575af505b1139bb81a5ba63d5d6053a5dbb69
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-08-29 21:11:00 +0200

    Feature Issue #68: Updated installation API

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\InstallationStatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,14 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DefaultController extends AbstractController
 {
-    #[Route('/{reactRouting}', name: 'app_default', requirements: ['reactRouting' => '(?!api).+'], defaults: ['reactRouting' => null])]
-    public function default(): Response
+    #[Route('/{reactRouting}', name: 'app_default', requirements: ['reactRouting' => '(?!api|install).+'], defaults: ['reactRouting' => null])]
+    public function default(InstallationStatusRepository $repository): Response
     {
+        $installationStatus = $repository->find(1);
+        if (!$installationStatus->isStatus()) {
+            return $this->redirectToRoute('api_install');
+        }
+
         return $this->render('default/index.html.twig');
     }
 }
