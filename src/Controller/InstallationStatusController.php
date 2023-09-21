3ca901e1ff4be2fa5cdc92da31f3cd3d947c7441
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-09-21 02:21:08 +0200

    Update Issue #143: Added new flag "update_v1_6" and string "version" to InstallationStatus; added DTO, Controller

---
+++ src/Controller/InstallationStatusController.php
@@ -0,0 +1,27 @@
+<?php namespace App\Controller;
+
+use App\DataTransferObject\InstallationStatusDTO;
+use App\Repository\InstallationStatusRepository;
+use App\Service\DtoResponseService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/installation-status')]
+class InstallationStatusController extends AbstractController
+{
+    public function __construct(
+        private readonly InstallationStatusRepository $installationStatusRepository,
+    ) {
+
+    }
+
+    #[Route('', name: 'api_installation_status_get')]
+    public function get(): Response
+    {
+        $installationStatus = $this->installationStatusRepository->find(1);
+        $dto = (new InstallationStatusDTO)->setUpdateV16($installationStatus->isUpdateV16())
+                                          ->setVersion($installationStatus->getVersion());
+        return DtoResponseService::getResponse($dto);
+    }
+}
