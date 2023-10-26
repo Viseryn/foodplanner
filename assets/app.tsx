939fd421d6e4ccaba50fcad302fafc2646432b0f
Kevin Sporbeck <kevin.sporbeck@gmail.com>
2023-10-26 23:18:19 +0200

    Moved version number to package.json

--- assets/app.tsx
+++ assets/app.tsx
@@ -1,20 +1,9 @@
-/*******************************
- * ./assets/app.tsx            *
- *******************************
- *
- * @author Kevin Sporbeck
- * @version v1.6-beta.3
- *
- */
-
-// Import CSS
 import './styles/app.scss'
-
-// Import React
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './layouts/App'
 
-// Render app
 const root = ReactDOM.createRoot(document.getElementById('root')!)
-root.render(<App />)
+root.render(
+    <App />
+)
