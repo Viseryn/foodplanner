cb61f4553f8f779c6f5d981ba15446d29c7d9dd7
Viseryn <info@yusel.net>
2024-10-22 19:16:48 +0200

    FP-61: Login is not possible now if the user is inactive

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Controller;
 
 use App\Component\Response\PrettyJsonResponse;
+use App\Service\UserControllerService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,10 +9,20 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    public function __construct(
+        private readonly UserControllerService  $userControllerService,
+    ) {}
+
     #[Route('/api/login', name: 'api_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
+
+        if ($error == null && !$this->userControllerService->getUser()->isActive()) {
+            return new PrettyJsonResponse([
+                'error' => "Account is not active. Please contact your administrator.",
+            ], 401);
+        }
 
         return new PrettyJsonResponse([
             'error' => $error,
