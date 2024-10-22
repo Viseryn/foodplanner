5d0560c4ac3b5f82736f45595dc3a7fd8ef29d29
Viseryn <info@yusel.net>
2024-10-22 16:51:33 +0200

    FP-62: Added ROLE_USER_ADMINISTRATION

--- src/Entity/Roles.php
+++ src/Entity/Roles.php
@@ -1,6 +1,19 @@
 <?php namespace App\Entity;
 
 enum Roles: string {
+    /**
+     * The standard role. Every user has this role, regardless of being active or not.
+     * The anonymous user does NOT have this role.
+     */
     case ROLE_USER = "ROLE_USER";
+
+    /**
+     * Only users with this role may use the app.
+     */
     case ROLE_ADMIN = "ROLE_ADMIN";
+
+    /**
+     * Users with this role can use the UserAdministration; i.e., changing other users' roles, user data, ...
+     */
+    case ROLE_USER_ADMINISTRATION = "ROLE_USER_ADMINISTRATION";
 }
