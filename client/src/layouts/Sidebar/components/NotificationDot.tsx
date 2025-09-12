420146d34d2eabca94ad1a3ca5552675636d19eb
Viseryn <info@yusel.net>
2025-09-12 20:18:24 +0200

    Issue #12: Updated positioning of notification dot in sidebar

--- client/src/layouts/Sidebar/components/NotificationDot.tsx
+++ client/src/layouts/Sidebar/components/NotificationDot.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react"
 
 export const NotificationDot = ({ value }: { value: number }): ReactElement => (
-    <div className="absolute inline-flex items-center justify-center -top-2 right-[2px] md:-top-[2px] md:-right-[2px] xl:top-[16px] xl:right-[16px] w-6 h-6 bg-red-500 border-2 border-bg dark:border-bg-dark xl:border-0 rounded-full z-40 font-bold text-xs text-white">
+    <div className="absolute inline-flex items-center justify-center top-0 right-0 transform translate-x-1 -translate-y-1 w-6 h-6 xl:top-5 xl:right-5 bg-red-500 border-2 border-bg dark:border-bg-dark xl:border-0 rounded-full z-40 font-bold text-xs text-white">
         {value}
     </div>
 )
